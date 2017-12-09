1c17ab224733e6261a2ea91371b4be6a113f2a15
Rudyard Richter <rudyardrichter@uchicago.edu>
2017-12-08 19:58:17 -0600

    feat(coverage): add empty setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fence',
+    version='0.1.0',
+    install_requires=[
+    ],
+    dependency_links=[
+    ],
+    scripts=[
+    ],
+    packages=find_packages(),
+)
