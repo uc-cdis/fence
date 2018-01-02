80bc231da4506e5d3240336c51810c43a94c16e6
avantol <avantol@uchicago.edu>
2018-01-02 12:39:40 -0600

    feat(google-creds-endpoint): create endpoints for listing, creating, and deleting temp Google creds

--- fence/blueprints/storage_creds.py
+++ fence/blueprints/storage_creds.py
@@ -1,9 +1,13 @@
-from ..auth import login_required
+from fence.auth import login_required
+from fence.data_model.models import GoogleServiceAccount
 import flask
+from cirrus import GoogleCloudManager
 from flask import current_app as capp
 from flask import g, request, jsonify
+from flask import abort
 from fence.resources.storage.cdis_jwt import create_refresh_token,\
     revoke_refresh_token, create_access_token
+from flask_sqlalchemy_session import current_session
 
 blueprint = flask.Blueprint('credentials', __name__)
 
@@ -11,7 +15,8 @@ ALL_RESOURCES = {
     "/cdis": "access to CDIS APIs",
     "/ceph": "access to Ceph storage",
     "/cleversafe": "access to cleversafe storage",
-    "/aws-s3": "access to AWS S3 storage"
+    "/aws-s3": "access to AWS S3 storage",
+    "/google": "access to Google storage"
 }
 
 
@@ -35,6 +40,7 @@ def list_sources():
             "/ceph": "access to Ceph storage",
             "/cleversafe": "access to cleversafe storage",
             "/aws-s3", "access to AWS S3 storage"
+            "/google", "access to Google Cloud storage"
         }
     '''
     services = capp.config.get('STORAGES', [])
@@ -63,9 +69,20 @@ def list_credentials(backend):
         ]
 
     '''
-    if backend != 'cdis':
+    if backend == 'cdis':
+        result = cdis_list_keypairs(g.user, current_session)
+    elif backend == 'google':
+        with GoogleCloudManager() as g_cloud_manager:
+            service_account = _get_google_service_account_for_client(g_cloud_manager)
+
+            if service_account:
+                result = g_cloud_manager.get_service_account_keys_info(service_account.google_unique_id)
+            else:
+                result = {}
+    else:
         result = capp.storage_manager.list_keypairs(backend, g.user)
-        return jsonify(dict(access_keys=result))
+
+    return jsonify(dict(access_keys=result))
 
 
 @blueprint.route('/<backend>/', methods=['POST'])
@@ -88,19 +105,39 @@ def create_credential(backend):
         {
             "token": "token_value"
         }
-        non-cdis:
+        google:
+        {
+            "type": "service_account",
+            "project_id": "project-id",
+            "private_key_id": "some_number",
+            "private_key": "-----BEGIN PRIVATE KEY-----\n....
+            =\n-----END PRIVATE KEY-----\n",
+            "client_email": "<api-name>api@project-id.iam.gserviceaccount.com",
+            "client_id": "...",
+            "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+            "token_uri": "https://accounts.google.com/o/oauth2/token",
+            "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+            "client_x509_cert_url": "https://www.googleapis.com/...<api-name>api%40project-id.iam.gserviceaccount.com"
+        }
+        other:
         {
             "access_key": "8DGW9LyC0D4nByoWo6pp",
             "secret_key": "1lnkGScEH8Vr4EC6QnoqLK1PqRWPNqIBJkH6Vpgx"
         }
     '''
+    client_id = getattr(g, "client_id", None)
     if backend == 'cdis':
         result = create_refresh_token(
             g.user, capp.keypairs[0],
             request.args.get('expire', 2592000),
-            request.args.get('scope', [])
+            request.args.get('scope', []),
+            client_id
         )
         return jsonify(dict(token=result))
+    elif backend == 'google':
+        with GoogleCloudManager() as g_cloud:
+            key = _get_google_access_key(g_cloud)
+        return jsonify(key)
     else:
         return jsonify(capp.storage_manager.create_keypair(backend, g.user))
 
@@ -125,20 +162,40 @@ def create_access_token_api(backend):
         {
             "access_token" "token_value"
         }
-        non-cdis:
+        google:
+        {
+            "type": "service_account",
+            "project_id": "project-id",
+            "private_key_id": "some_number",
+            "private_key": "-----BEGIN PRIVATE KEY-----\n....
+            =\n-----END PRIVATE KEY-----\n",
+            "client_email": "<api-name>api@project-id.iam.gserviceaccount.com",
+            "client_id": "...",
+            "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+            "token_uri": "https://accounts.google.com/o/oauth2/token",
+            "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+            "client_x509_cert_url": "https://www.googleapis.com/...<api-name>api%40project-id.iam.gserviceaccount.com"
+        }
+        other:
         {
             "access_key": "8DGW9LyC0D4nByoWo6pp",
             "secret_key": "1lnkGScEH8Vr4EC6QnoqLK1PqRWPNqIBJkH6Vpgx"
         }
     '''
+    client_id = getattr(g, "client_id", None)
     if backend == 'cdis':
         result = create_access_token(
             g.user, capp.keypairs[0],
             request.form['refresh_token'],
             request.args.get('expire', 2592000),
-            request.args.get('scope', [])
+            request.args.get('scope', []),
+            client_id
         )
         return jsonify(dict(access_token=result))
+    elif backend == 'google':
+        with GoogleCloudManager() as g_cloud:
+            key = _get_google_access_key(g_cloud)
+        return jsonify(key)
     else:
         return jsonify(capp.storage_manager.create_keypair(backend, g.user))
 
@@ -148,16 +205,131 @@ def create_access_token_api(backend):
 def delete_credential(backend, access_key):
     '''
     .. http:get: /hmac/(string: access_key)
-    Revoke a credential (keypair/token) for user
+    Revoke a credential for user
 
     :param access_key: existing access key belongs to this user
 
     :statuscode 201 Success
-    :statuscode 404 access key doesn't exist
+    :statuscode 403 Forbidden to delete access key
+    :statuscode 404 Access key doesn't exist
 
     '''
     if backend == 'cdis':
-        return revoke_refresh_token(access_key)
+        status = revoke_refresh_token(access_key)
+    elif backend == 'google':
+        with GoogleCloudManager() as g_cloud:
+            service_account = _get_google_service_account_for_client(g_cloud)
+
+            if service_account:
+                keys_for_account = (
+                    g_cloud.get_service_account_keys_info(service_account.google_unique_id)
+                )
+
+                # Only delete the requested key if is owned by current client's SA
+                if access_key in [key["name"].split("/")[-1] for key in keys_for_account]:
+                    g_cloud.delete_service_account_key(service_account.google_unique_id,
+                                                       access_key)
+                    status = '', 201
+                else:
+                    abort(403, "Cannot delete keys you don't own.")
+            else:
+                abort(404, "Could not find service account for current user.")
     else:
         capp.storage_manager.delete_keypair(backend, g.user, access_key)
-    return '', 201
+        status = '', 201
+
+    return status
+
+
+def _get_google_access_key(g_cloud_manager):
+    """
+    Return an access key for current user and client.
+
+    Args:
+        g_cloud_manager (cirrus.GoogleCloudManager): instance of
+        cloud manager to use
+
+    Returns:
+        .. code-block:: JavaScript
+
+            {
+                "type": "service_account",
+                "project_id": "project-id",
+                "private_key_id": "some_number",
+                "private_key": "-----BEGIN PRIVATE KEY-----\n....
+                =\n-----END PRIVATE KEY-----\n",
+                "client_email": "<api-name>api@project-id.iam.gserviceaccount.com",
+                "client_id": "...",
+                "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+                "token_uri": "https://accounts.google.com/o/oauth2/token",
+                "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+                "client_x509_cert_url": "https://www.googleapis.com/...<api-name>api%40project-id.iam.gserviceaccount.com"
+            }
+    """
+    service_account = _get_google_service_account_for_client(g_cloud_manager)
+
+    if not service_account:
+        service_account = _create_google_service_account_for_client(g_cloud_manager)
+
+    key = g_cloud_manager.get_access_key(service_account.google_unique_id)
+    return key
+
+
+def _get_google_service_account_for_client(g_cloud_manager):
+    """
+    Return the service account (from Fence db) for current client.
+
+    Get the service account that is associated with the current client
+    for this user. There will be a single service account per client.
+
+    NOTE: The user themselves will also have a single service account
+          which will be used when "client_id" is their user.id
+
+    Args:
+        g_cloud_manager (cirrus.GoogleCloudManager): instance of
+        cloud manager to use
+
+    Returns:
+        fence.data_model.models.GoogleServiceAccount: Client's service account
+    """
+    client_id = getattr(g, "client_id", None)
+    service_account = (
+        current_session
+        .query(GoogleServiceAccount)
+        .filter_by(client_id=client_id,
+                   user_id=g.user.id)
+        .first()
+    )
+
+    return service_account
+
+
+def _create_google_service_account_for_client(g_cloud_manager):
+    """
+    Create a Google Service account for the current client and user.
+
+    Args:
+        g_cloud_manager (cirrus.GoogleCloudManager): instance of
+        cloud manager to use
+
+    Returns:
+        fence.data_model.models.GoogleServiceAccount: New service account
+    """
+    # create service account, add to db
+    user_google_group = g.user.google_group_id
+    client_id = getattr(g, "client_id", None)
+    new_service_account = (
+        g_cloud_manager.create_service_account_for_proxy_group(user_google_group,
+                                                               account_id=client_id)
+    )
+    # TODO: catch 409 (account already exists) and attempt to retrieve and add to db?
+    service_account = GoogleServiceAccount(
+        google_unique_id=new_service_account["uniqueId"],
+        client_id=client_id,
+        user_id=g.user.id,
+        email=new_service_account["email"]
+    )
+    current_session.add(service_account)
+    current_session.commit()
+
+    return service_account
