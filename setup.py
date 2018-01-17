d3a89660abeb7de6ca3ed1e9809593e557cfe6fb
Giang Bui <giangbui@uchicago.edu>
2018-01-17 15:53:10 -0600

    fix(python.setup): bump cdispyutil version

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
         "git+https://github.com/uc-cdis/flask-postgres-session.git@68bf5a9723a351729855c429eca8a0f4bbb830c7#egg=flask_postgres_session-0.1.3",
         "git+https://github.com/uc-cdis/storage-client.git@0.1.5#egg=storageclient-0.1.4",
         "git+https://github.com/uc-cdis/userdatamodel.git@1.0.2#egg=userdatamodel",
-        "git+https://github.com/uc-cdis/cdis-python-utils.git@0.2.1#egg=cdispyutils",
+        "git+https://github.com/uc-cdis/cdis-python-utils.git@0.2.3#egg=cdispyutils",
         "git+https://github.com/uc-cdis/cirrus.git@1a40b1518606d3963f6d40f8ac611fde1d95c814#egg=cirrus",
     ],
     scripts=[
