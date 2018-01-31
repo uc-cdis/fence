b18f737f808800e7f6702ca264dc39000664a6fb
Phuong Pham <phuongpl@gmail.com>
2018-01-31 22:43:26 +0000

    Change the list

--- fence/resources/group/__init__.py
+++ fence/resources/group/__init__.py
@@ -3,11 +3,13 @@ from flask import jsonify, g
 
 from fence.data_model.models import ResearchGroup
 
+
 def find_group(group_id, session):
     group = session.query(ResearchGroup).filter(ResearchGroup.id == group_id).first()
     if not group:
         raise NotFound("group {} not found".format(group_id))
     return group
+
 
 def get_group_info(group):
     info = {
@@ -15,7 +17,8 @@ def get_group_info(group):
         'group_name': group.name,
         'lead_id': group.lead_id
     }
-    return jsonify(info)
+    return info
+
 
 def get_info_by_group_id(group_id):
     with capp.db.session as session:
@@ -23,14 +26,12 @@ def get_info_by_group_id(group_id):
 
 
 def get_all_groups_info():
+
+    all_info = list()
     with capp.db.session as session:
         groups = session.query(ResearchGroup).all()
-        if groups is None:
-            raise NotFound("group {} not found".format(group_id))
-
-        all_info = list()
 
         for group in groups:
             all_info.append(get_group_info(group))
 
-        return all_info
+    return json.dumps(all_info)
