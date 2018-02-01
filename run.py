ac1a3d9565892996f6ff7ce94bde8cec2814fddc
Jesus Pedrosa <jesuspg@uchicago.edu>
2018-02-01 23:35:54 +0000

    feat(chore): fix mock dependency when not needed

--- run.py
+++ run.py
@@ -1,10 +1,10 @@
 from fence import app, app_config, app_sessions
-from mock import patch
-from cdisutilstest.code.storage_client_mock import get_client
-
 app_config(app)
 
 if app.config.get('MOCK_STORAGE', False):
+    from mock import patch
+    from cdisutilstest.code.storage_client_mock import get_client
+
     patcher = patch(
         'fence.resources.storage.get_client',
         get_client)
