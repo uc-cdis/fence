4273fca81d6fab244d79690e191cd40f6e348930
avantol <avantol@uchicago.edu>
2018-03-01 12:46:49 -0600

    feat(db-migrations): add migrations for google tables and proxy group

--- fence/models/_migrate.py
+++ fence/models/_migrate.py
@@ -4,9 +4,14 @@ database migrations.
 """
 from sqlalchemy import Table
 from sqlalchemy import MetaData
+from sqlalchemy.schema import ForeignKey
+from sqlalchemy import String
 
 from fence.jwt.token import CLIENT_ALLOWED_SCOPES
 
+from fence.models.users import User
+from fence.models.cloud_resources import GoogleProxyGroup
+from fence.models.cloud_resources import GoogleServiceAccount
 from fence.models.auth import Client
 from fence.models.creds import UserRefreshToken
 
@@ -64,28 +69,114 @@ def migrate(driver):
                 .format(Client.__tablename__)
             )
 
-#TODO
-# -    user_id = Column(
-# -        Integer,
-# -        ForeignKey(User.id),
-# -        nullable=False,
-# -        unique=True
-# +    email = Column(
-# +        String,
-# +        unique=True,
-# +        nullable=False
-#      )
-# -    user = relationship('User')
+    add_column_if_not_exist(
+        table_name=GoogleProxyGroup.__tablename__,
+        column_name='email',
+        column_type=String,
+        driver=driver,
+        metadata=md
+    )
 
-# TODO removed client_id fk restriction
-# -    client_id = Column(
-# -        String(40),
-# -        ForeignKey('client.client_id')
-# -    )
-# -    client = relationship('Client')
-# +    client_id = Column(String(40))
+    drop_foreign_key_column_if_exist(
+        table_name=GoogleProxyGroup.__tablename__,
+        column_name='user_id',
+        driver=driver,
+        metadata=md
+    )
+
+    add_foreign_key_column_if_not_exist(
+        table_name=User.__tablename__,
+        column_name='google_proxy_group_id',
+        column_type=String(90),
+        fk_table_name=GoogleProxyGroup.__tablename__,
+        fk_column_name='id',
+        driver=driver,
+        metadata=md
+    )
+
+    drop_foreign_key_constraint_if_exist(
+        table_name=GoogleServiceAccount.__tablename__,
+        column_name='client_id',
+        driver=driver,
+        metadata=md
+    )
 
 
-# TODO added
-# +    google_proxy_group_id = Column(String(90), ForeignKey('google_proxy_group.id'))
-# +    google_proxy_group = relationship('GoogleProxyGroup', backref=backref(__tablename__, uselist=False))
+def add_foreign_key_column_if_not_exist(
+        table_name, column_name, column_type, fk_table_name, fk_column_name, driver,
+        metadata):
+    add_column_if_not_exist(
+        table_name, column_name, column_type, driver, metadata)
+    add_foreign_key_constraint_if_not_exist(
+        table_name, column_name, fk_table_name, fk_column_name, driver,
+        metadata)
+
+
+def drop_foreign_key_column_if_exist(table_name, column_name, driver, metadata):
+    drop_foreign_key_constraint_if_exist(
+        table_name, column_name, driver, metadata)
+    drop_column_if_exist(table_name, column_name, driver, metadata)
+
+
+def add_column_if_not_exist(
+        table_name, column_name, column_type, driver, metadata):
+    table = Table(
+        table_name, metadata, autoload=True, autoload_with=driver.engine)
+    if column_name not in table.c:
+        with driver.session as session:
+            session.execute(
+                "ALTER TABLE \"{}\" ADD COLUMN {} {};"
+                .format(table_name, column_name, column_type)
+            )
+            session.commit()
+
+
+def drop_column_if_exist(table_name, column_name, driver, metadata):
+    table = Table(
+        table_name, metadata, autoload=True, autoload_with=driver.engine)
+    if column_name in table.c:
+        with driver.session as session:
+            session.execute(
+                "ALTER TABLE \"{}\" DROP COLUMN {};"
+                .format(table_name, column_name)
+            )
+            session.commit()
+
+
+def add_foreign_key_constraint_if_not_exist(
+        table_name, column_name, fk_table_name, fk_column_name,
+        driver, metadata):
+    table = Table(
+        table_name, metadata, autoload=True, autoload_with=driver.engine)
+    foreign_key_name = "{}_{}_fkey".format(table_name.lower(), column_name)
+
+    if column_name in table.c:
+        foreign_keys = [fk.name for fk in getattr(table.c, column_name).foreign_keys]
+        if foreign_key_name not in foreign_keys:
+            with driver.session as session:
+                session.execute(
+                    "ALTER TABLE \"{}\" ADD CONSTRAINT {} "
+                    "FOREIGN KEY({}) REFERENCES {} ({});"
+                    .format(
+                        table_name, foreign_key_name, column_name,
+                        fk_table_name, fk_column_name
+                    )
+                )
+                session.commit()
+
+
+def drop_foreign_key_constraint_if_exist(
+        table_name, column_name, driver, metadata):
+    table = Table(
+        table_name, metadata, autoload=True, autoload_with=driver.engine)
+    foreign_key_name = "{}_{}_fkey".format(table_name.lower(), column_name)
+
+    if column_name in table.c:
+        foreign_keys = [fk.name for fk in getattr(table.c, column_name).foreign_keys]
+        if foreign_key_name in foreign_keys:
+            with driver.session as session:
+                session.execute(
+                    "ALTER TABLE \"{}\" DROP CONSTRAINT {};"
+                    .format(table_name, foreign_key_name)
+                )
+                session.commit()
