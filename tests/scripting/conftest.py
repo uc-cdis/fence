c1c7fb2fcb756f2b2fa4a76b9e6a508fbb1f960e
Rudyard Richter <rudyardrichter@uchicago.edu>
2018-03-02 19:12:01 -0600

    feat(remove-user): patch db to fix test

--- tests/scripting/conftest.py
+++ tests/scripting/conftest.py
@@ -3,11 +3,19 @@ import pytest
 from fence.models import User
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope='module')
 def example_usernames():
     return ['A', 'B', 'C']
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope='module')
 def example_users(example_usernames):
     return [User(username=username) for username in example_usernames]
+
+
+@pytest.fixture(scope='function', autouse=True)
+def patch_driver(db, monkeypatch):
+    monkeypatch.setattr(
+        'fence.scripting.fence_create.SQLAlchemyDriver',
+        lambda _: db,
+    )
--- tests/scripting/test_fence-create.py
+++ tests/scripting/test_fence-create.py
@@ -17,7 +17,6 @@ def test_delete_users(app, db_session, example_usernames, example_users):
     # Get the list of usernames for users that still exist.
     # (The `list(zip(...))` trick is to turn a list of 1-tuples into a
     # flattened list.)
-    import pdb; pdb.set_trace()
     remaining_usernames = list(zip(*db_session.query(User.username).all())[0])
     assert example_usernames[0] in remaining_usernames
     for username in example_usernames[1:]:
