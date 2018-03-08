230ee374d9df75021fa9aa3ce3ac68055eda2314
Jesus Pedrosa <jesuspg@uchicago.edu>
2018-03-08 02:54:11 +0000

    feat(test): add user tests

--- tests/admin/test_admin_users.py
+++ tests/admin/test_admin_users.py
@@ -1,5 +1,8 @@
 import fence.resources.admin as adm
 from fence.models import User
+import pytest
+from fence.errors import NotFound
+
 
 def test_get_user(db_session, awg_users):
     info = adm.get_user_info(db_session, "awg_user")
@@ -15,7 +18,34 @@ def test_get_user(db_session, awg_users):
     assert info['project_access']['phs_project_2'] == ['read']
 
 
-def test_add_user(db_session):
-    adm.create_user(db_session, "insert_user", "insert_user@fake.com", "admin")
+def test_create_user(db_session):
+    adm.create_user(db_session, "insert_user", "admin", "insert_user@fake.com")
     user = db_session.query(User).filter(User.username == "insert_user").first()
     assert user.username == "insert_user"
+    assert user.is_admin == True
+    assert user.email == "insert_user@fake.com"
+
+
+def test_delete_user(db_session, awg_users):
+    user = db_session.query(User).filter(User.username == "awg_user").first()
+    assert user != None
+    adm.delete_user(db_session, "awg_user")
+    user = db_session.query(User).filter(User.username == "awg_user").first()
+    assert user == None
+
+
+def test_update_user(db_session, awg_users):
+    user = db_session.query(User).filter(User.username == "awg_user").first()
+    assert user != None
+    adm.update_user(db_session, "awg_user", "admin", "new_email@fake.com", "new_awg_user")
+    user = db_session.query(User).filter(User.username == "awg_user").first()
+    assert user == None
+    user = db_session.query(User).filter(User.username == "new_awg_user").first()
+    assert user.username == "new_awg_user"
+    assert user.is_admin == True
+    assert user.email == "new_email@fake.com"
+
+
+def test_get_inexistent_user(db_session):
+    with pytest.raises(NotFound):
+        adm.get_user_info(db_session, "nonenone")
