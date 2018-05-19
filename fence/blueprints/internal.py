4398cee48830498f7e485a133955c593da8bbf9a
Zac Flamig <Zac@WeatherWary.com>
2018-05-19 11:40:43 -0500

    fix(codacy): code cleanup

--- fence/blueprints/internal.py
+++ fence/blueprints/internal.py
@@ -1,5 +1,3 @@
-import re
-
 import flask
 
 HAS_KUBERNETES = True
@@ -15,8 +13,8 @@ from fence.resources.user import find_user
 
 blueprint = flask.Blueprint('internal', __name__)
 
-POD_USERNAME_LABEL = 'gen3username'
-JUPYTER_POD_LABEL = 'hub.jupyter.org/username'
+POD_USERNAME_ANNOTATION = 'gen3username'
+JUPYTER_POD_ANNOTATION = 'hub.jupyter.org/username'
 
 @blueprint.route('/access_token', methods=['GET'])
 def internal_access_token():
@@ -30,7 +28,7 @@ def internal_access_token():
     # circuits requests that come in publicly
     if 'X-Forwarded-For' in flask.request.headers:
         flask.abort(404)
-   
+
     ip = flask.request.remote_addr
     username = get_username_from_ip(ip)
     if not username:
@@ -40,8 +38,8 @@ def internal_access_token():
     keypair = flask.current_app.keypairs[0]
     scopes = ['fence', 'openid', 'user', 'data']
    
-    with flask.current_app.db.session as session: 
-        user = find_user(username, session) 
+    with flask.current_app.db.session as session:
+        user = find_user(username, session)
         access_token = token.generate_signed_access_token(
             keypair.kid, keypair.private_key, user, expires_in, scopes
         )
@@ -53,15 +51,15 @@ def get_username_from_ip(ip):
     # Fail if we can't load kubernetes config...
     try:
         kubernetes.config.load_incluster_config()
-    except Exception as e:
+    except Exception:
         return None
     v1 = kubernetes.client.CoreV1Api()
     ret = v1.list_pod_for_all_namespaces(field_selector='status.podIP={}'.format(ip), watch=False)
     for pod in ret.items:
-        if pod.metadata.annotations and POD_USERNAME_LABEL in pod.metadata.annotations:
-            return pod.metadata.annotations[POD_USERNAME_LABEL]
-        elif pod.metadata.annotations and JUPYTER_POD_LABEL in pod.metadata.annotations:
-            return pod.metadata.annotations[JUPYTER_POD_LABEL]
+        if pod.metadata.annotations and POD_USERNAME_ANNOTATION in pod.metadata.annotations:
+            return pod.metadata.annotations[POD_USERNAME_ANNOTATION]
+        elif pod.metadata.annotations and JUPYTER_POD_ANNOTATION in pod.metadata.annotations:
+            return pod.metadata.annotations[JUPYTER_POD_ANNOTATION]
 
     # No matching pod found
     return None
--- tests/test_internal.py
+++ tests/test_internal.py
@@ -2,17 +2,17 @@ from . import utils
 import urlparse
 import pytest
 
-def test_fail_internal_access_key(client, auth_client):
+def test_fail_internal_access_token(client, auth_client):
     """
-    Test ``GET /internal/access_key``.
+    Test ``GET /internal/access_token``.
     """
     path = '/internal/access_token'
     response = client.get(path, headers={"X-Forwarded-For":"127.0.0.1"})
     assert response.status_code == 404
 
-def test_unauthorized_internal_access_key(client, auth_client):
+def test_unauthorized_internal_access_token(client, auth_client):
     """
-    Test ``GET /internal/access_key``.
+    Test ``GET /internal/access_token``.
     """
     path = '/internal/access_token'
     response = client.get(path)
