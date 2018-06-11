c05bca3ef86db9ef2272064d00db479a1baccbb3
benJrohrer <benJrohrer@gmail.com>
2018-06-11 15:12:04 -0500

    Added tables for the Service Account Registration feature. #240 (#245)
    * Added tables for the Service Account Registration feature.
    
    * Added UserServiceAccount, ServiceAccountAccessPrivilege and ServiceAccountToGoogleBucketAccessGroup Tables. Added unit test in test_datamodel.py to test relationships between/within new tables and existing tables (GoogleAccessBucketGroup and Project).
    
    * Fixed style according to Alex's review
    
    * Style fixes, indentation

--- tests/test_datamodel.py
+++ tests/test_datamodel.py
@@ -1,4 +1,12 @@
-from fence.models import User, Client
+from fence.models import (
+    Client,
+    GoogleBucketAccessGroup,
+    ServiceAccountAccessPrivilege,
+    ServiceAccountToGoogleBucketAccessGroup,
+    User,
+    UserServiceAccount
+)
+from userdatamodel.user import Bucket, Project
 from fence.utils import random_str
 
 
@@ -19,3 +27,62 @@ def test_user_delete_cascade(db_session):
         db_session.query(Client).filter_by(client_id=client.client_id).count()
         == 0
     )
+
+
+def test_service_account_relationsips(db_session):
+    """
+    test service account tables have proper relationships/fields
+    """
+    project = Project(id=1)
+    bucket = Bucket(id=1)
+    user_sa = UserServiceAccount(
+        id=1,
+        google_unique_id="guid",
+        email="email@google.com",
+        google_project_id="gpid"
+    )
+    sa_access_privilege = ServiceAccountAccessPrivilege(
+        id=1,
+        project_id=1,
+        service_account_id=1
+    )
+    gbag = GoogleBucketAccessGroup(
+        id=1,
+        bucket_id=1,
+        email="email@google.com")
+    sa_to_gbag = ServiceAccountToGoogleBucketAccessGroup(
+        id=1,
+        service_account_id=1,
+        expires=0,
+        access_group_id=1)
+    db_session.add(project)
+    db_session.add(bucket)
+    db_session.add(user_sa)
+    db_session.add(sa_access_privilege)
+    db_session.add(gbag)
+    db_session.add(sa_to_gbag)
+    db_session.commit()
+    assert (
+        project.sa_access_privileges[0].__class__
+        == ServiceAccountAccessPrivilege)
+    assert project.sa_access_privileges[0].id == 1
+    assert sa_access_privilege.project.__class__ == Project
+    assert sa_access_privilege.project.id == 1
+    assert sa_access_privilege.service_account.__class__ == UserServiceAccount
+    assert sa_access_privilege.service_account.id == 1
+    assert (
+        user_sa.access_privileges[0].__class__ 
+        == ServiceAccountAccessPrivilege)
+    assert user_sa.access_privileges[0].id == 1
+    assert (
+        user_sa.to_access_groups[0].__class__
+        == ServiceAccountToGoogleBucketAccessGroup)
+    assert user_sa.to_access_groups[0].id == 1
+    assert sa_to_gbag.service_account.__class__ == UserServiceAccount
+    assert sa_to_gbag.service_account.id == 1
+    assert sa_to_gbag.access_group.__class__ == GoogleBucketAccessGroup
+    assert sa_to_gbag.access_group.id == 1
+    assert (
+        gbag.to_access_groups[0].__class__
+        == ServiceAccountToGoogleBucketAccessGroup)
+    assert gbag.to_access_groups[0].id == 1
