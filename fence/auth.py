2700034a9d20cfb32c707932085d32f526910711
Benjamin Rohrer <benJrohrer@gmail.com>
2018-06-12 15:19:16 -0500

    formatting

--- fence/auth.py
+++ fence/auth.py
@@ -55,8 +55,6 @@ def login_user(request, username, provider):
 
 
 def logout(next_url=None):
-    # Call get_current_user (but ignore the result) just to check that either
-    # the user is logged in or that authorization is mocked.
     flask.current_app.logger.debug("IN AUTH LOGOUT, next_url = {0}".format(next_url))
     itrust_next_url = None
     if flask.session.get('provider') == IdentityProvider.itrust:
