a42c017f828c806a2902aef425832d3e15e16bc5
Alexander VanTol <Avantol13@users.noreply.github.com>
2018-07-05 09:03:17 -0500

    Feat/error with redirect (#280)
    * feat(error-handling): redirect with errors if redirect arg in request
    
    * fix(error-handling): check session for redirect too
    
    * chore(imports): remove unused
    
    * fix(error-handling): correctly parse parent module for clear error name
    
    * chore(linking): use utility function
    
    * chore(tests): update tests to check redirect without query params
    
    * fix(tests): update tests to only check necessary url

--- fence/utils.py
+++ fence/utils.py
@@ -6,6 +6,9 @@ from random import SystemRandom
 import re
 import string
 from sqlalchemy import func
+from urllib import urlencode
+from urlparse import parse_qs, urlsplit, urlunsplit
+
 import flask
 from userdatamodel.driver import SQLAlchemyDriver
 from werkzeug.datastructures import ImmutableMultiDict
@@ -144,3 +147,42 @@ def clear_cookies(response):
     """
     for cookie_name in flask.request.cookies.values():
         response.set_cookie(cookie_name, '', expires=0)
+
+
+def get_error_params(error, description):
+    params = ''
+    if error:
+        args = {'error': error, 'error_description': description}
+        params = urlencode(args)
+    return params
+
+
+def append_query_params(original_url, **kwargs):
+    """
+    Add additional query string arguments to the given url.
+
+    Example call:
+        new_url = append_query_params(
+            original_url, error='this is an error',
+            another_arg='this is another argument')
+    """
+    scheme, netloc, path, query_string, fragment = urlsplit(original_url)
+    query_params = parse_qs(query_string)
+    if kwargs is not None:
+        for key, value in kwargs.iteritems():
+            query_params[key] = [value]
+
+    new_query_string = urlencode(query_params, doseq=True)
+    new_url = (
+        urlunsplit((scheme, netloc, path, new_query_string, fragment))
+    )
+    return new_url
+
+
+def split_url_and_query_params(url):
+    scheme, netloc, path, query_string, fragment = urlsplit(url)
+    query_params = parse_qs(query_string)
+    url = (
+        urlunsplit((scheme, netloc, path, None, fragment))
+    )
+    return url, query_params
