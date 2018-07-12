dfccce93152b5aeda60d633696f9fc81eb6d6bce
Rudyard Richter <rudyardrichter@uchicago.edu>
2018-07-12 15:27:21 -0500

    feat(roles): add put/delete & refactor

--- fence/blueprints/rbac.py
+++ fence/blueprints/rbac.py
@@ -10,7 +10,7 @@ see if the user has roles allowing them to use these endpoints.
 import flask
 
 from fence.auth import admin_login_required
-from fence.errors import InternalError, NotFound, UserError
+from fence.errors import NotFound, UserError
 from fence.models import Policy, User
 
 
@@ -32,35 +32,66 @@ def _get_user(user_id):
     return user
 
 
+def lookup_policies(policy_ids):
+    """
+    Look up the list of policies from the database.
 
-@blueprint.route('/role/', methods=['GET'])
-@admin_login_required
-def list_roles():
+    Requires flask application context.
+
+    Args:
+        policy_ids (List[str]): list of IDs for the policies to return
+
+    Return:
+        List[fence.model.Policy]: list of policy models
+
+    Raises:
+        - ValueError: if any of the policy IDs do not correspond to an existing
+          policy
     """
-    List all the existing roles.
-    """
-    return flask.jsonify(flask.current_app.arborist.list_roles())
+    policies = []
+    with flask.current_app.db.session as session:
+        for policy_id in policy_ids:
+            policy = session.query(Policy).filter_by(ID=policy_id).first()
+            if not policy:
+                raise NotFound(
+                    'policy not registered in fence: {}'
+                    .format(policy_id)
+                )
+            policies.append(policy)
+    return policies
 
 
-@blueprint.route('/role/', methods=['POST'])
-@admin_login_required
-def create_role():
+def _validate_policy_ids(policy_ids):
     """
-    Create a new role.
-    """
-    data = flask.request.get_json()
-    return flask.jsonify(flask.current_app.arborist.create_role(data))
+    Check some user-inputted policy IDs which should correspond to roles in
+    arborist.
 
+    Check:
+        - Policies argument is there
+        - All the listed policies are valid
+            - Contain correct fields
+            - Actually exist in arborist
 
-@blueprint.route('/role/<role_id>', methods=['GET', 'DELETE', 'PATCH', 'PUT'])
-@admin_login_required
-def role_operation(role_id):
+    Args:
+        policy_ids (List[str]): list of policy IDs
+
+    Return:
+        List[str]: the same policy_ids, if they validated
+
+    Raises:
+        UserError: if the policy ID list fails to validate
     """
-    Handle read, update, append, and delete operations on an existing role.
-    """
-    return flask.jsonify(flask.current_app.arborist.role_request(
-        role_id, method=flask.request.method, json=flask.request.get_json()
-    ))
+    if not policy_ids:
+        raise UserError('JSON missing required value `policies`')
+    missing_policies = flask.current_app.arborist.policies_not_exist(
+        policy_ids
+    )
+    if any(missing_policies):
+        raise UserError(
+            'policies with these IDs do not exist in arborist: {}'
+            .format(missing_policies)
+        )
+    return policy_ids
 
 
 @blueprint.route('/policy/', methods=['GET'])
@@ -124,39 +155,46 @@ def grant_policy_to_user(user_id):
     """
     Grant additional policies to a user.
     """
-    # Input validation:
-    #     - Policies argument is there
-    #     - All the listed policies are valid
-    #         - Contain correct fields
-    #         - Actually exist in arborist
-    policy_ids = flask.request.get_json().get('policies')
-    if not policy_ids:
-        raise UserError('JSON missing required value `policies`')
-    missing_policies = flask.current_app.arborist.policies_not_exist(
-        policy_ids
-    )
-    if any(missing_policies):
-        raise UserError(
-            'policies with these IDs do not exist in arborist: {}'
-            .format(missing_policies)
-        )
+    policy_ids = _validate_policy_ids(flask.request.get_json().get('policies'))
 
     with flask.current_app.db.session as session:
         user = session.query(User).filter(User.id == user_id).first()
         if not user:
             raise NotFound('no user exists with ID: {}'.format(user_id))
+        policies = lookup_policies(policy_ids)
+        user.policies.extend(policies)
+        session.commit()
 
-        policies_to_grant = []
-        for policy_id in policy_ids:
-            policy = session.query(Policy).filter_by(ID=policy_id).first()
-            if not policy:
-                raise InternalError(
-                    'policy not registered in fence: {}'
-                    .format(policy_id)
-                )
-            policies_to_grant.append(policy)
+    return '', 204
 
-        for policy in policies_to_grant:
-            user.policies.append(policy)
 
-    return flask.jsonify({'granted': policy_ids})
+@blueprint.route('/user/<user_id>/policies/', methods=['PUT'])
+@admin_login_required
+def replace_user_policies(user_id):
+    """
+    Overwrite the user's existing policies and replace them with the ones
+    provided in the request.
+    """
+    policy_ids = _validate_policy_ids(flask.request.get_json().get('policies'))
+
+    with flask.current_app.db.session as session:
+        user = _get_user(user_id)
+        policies = lookup_policies(policy_ids)
+        user.policies = policies
+        session.commit()
+
+    return '', 204
+
+
+@blueprint.route('/user/<user_id>/policies/', methods=['DELETE'])
+@admin_login_required
+def revoke_user_policies(user_id):
+    """
+    Revoke all the policies which this user has access to.
+    """
+    with flask.current_app.db.session as session:
+        user = _get_user(user_id)
+        # Set user's policies to empty list.
+        user.policies = []
+        session.commit()
+    return '', 204
