ce1740048f0cea4b6858cb5e94bb4ac8e80518c6
Benjamin Rohrer <benJrohrer@gmail.com>
2018-07-13 12:48:03 -0500

    *forgot to include test file

---
+++ tests/google/test_access_utils.py
@@ -0,0 +1,30 @@
+from fence.resources.google.access_utils import (
+    is_valid_service_account_type,
+)
+
+
+def test_is_valid_service_account(cloud_manager):
+
+    (
+        cloud_manager.return_value.__enter__.
+        return_value.get_service_account_type.return_value
+    ) = "COMPUTE_ENGINE_DEFAULT"
+    assert is_valid_service_account_type(cloud_manager.project_id, 1)
+
+    (
+        cloud_manager.return_value.__enter__.
+        return_value.get_service_account_type.return_value
+    ) = "GOOGLE_API"
+    assert not is_valid_service_account_type(cloud_manager.project_id, 1)
+
+    (
+        cloud_manager.return_value.__enter__.
+        return_value.get_service_account_type.return_value
+    ) = "COMPUTE_ENGINE_API"
+    assert not is_valid_service_account_type(cloud_manager.project_id, 1)
+
+    (
+        cloud_manager.return_value.__enter__.
+        return_value.get_service_account_type.return_value
+    ) = "USER_MANAGED"
+    assert is_valid_service_account_type(cloud_manager.project_id, 1)
