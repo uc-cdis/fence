8627492556227ea131af97ad8e2046a8f81cf117
Benjamin Rohrer <benJrohrer@gmail.com>
2018-07-13 14:09:54 -0500

    *updated service account type variables *pointed this branch of fence at required cirrus branch

--- tests/google/test_access_utils.py
+++ tests/google/test_access_utils.py
@@ -2,10 +2,10 @@ from fence.resources.google.access_utils import (
     is_valid_service_account_type,
 )
 from cirrus.google_cloud import (
-    COMPUTE_ENGINE_DEFAULT,
-    COMPUTE_ENGINE_API,
-    GOOGLE_API,
-    USER_MANAGED,
+    COMPUTE_ENGINE_DEFAULT_SERVICE_ACCOUNT,
+    COMPUTE_ENGINE_API_SERVICE_ACCOUNT,
+    GOOGLE_API_SERVICE_ACCOUNT,
+    USER_MANAGED_SERVICE_ACCOUNT,
 )
 
 
@@ -14,23 +14,23 @@ def test_is_valid_service_account(cloud_manager):
     (
         cloud_manager.return_value.__enter__.
         return_value.get_service_account_type.return_value
-    ) = COMPUTE_ENGINE_DEFAULT
+    ) = COMPUTE_ENGINE_DEFAULT_SERVICE_ACCOUNT
     assert is_valid_service_account_type(cloud_manager.project_id, 1)
 
     (
         cloud_manager.return_value.__enter__.
         return_value.get_service_account_type.return_value
-    ) = GOOGLE_API
+    ) = GOOGLE_API_SERVICE_ACCOUNT
     assert not is_valid_service_account_type(cloud_manager.project_id, 1)
 
     (
         cloud_manager.return_value.__enter__.
         return_value.get_service_account_type.return_value
-    ) = COMPUTE_ENGINE_API
+    ) = COMPUTE_ENGINE_API_SERVICE_ACCOUNT
     assert not is_valid_service_account_type(cloud_manager.project_id, 1)
 
     (
         cloud_manager.return_value.__enter__.
         return_value.get_service_account_type.return_value
-    ) = USER_MANAGED
+    ) = USER_MANAGED_SERVICE_ACCOUNT
     assert is_valid_service_account_type(cloud_manager.project_id, 1)
