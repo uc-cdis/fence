9b131b4fda5a9b984ef1372c312f4f77aa21002a
Reuben Pasquini <reuben@frickjack.com>
2018-08-15 10:06:00 -0500

    fix(dockerrun): bad bash if

--- dockerrun.bash
+++ dockerrun.bash
@@ -8,7 +8,7 @@ update-ca-certificates
 #
 # Enable debug flag based on GEN3_DEBUG environment
 #
-if [ -f ./wsgi.py && "$GEN3_DEBUG" == "True" ]; then
+if [[ -f ./wsgi.py && "$GEN3_DEBUG" == "True" ]]; then
   echo -e "\napplication.debug=True\n" >> ./wsgi.py
 fi  
 #
