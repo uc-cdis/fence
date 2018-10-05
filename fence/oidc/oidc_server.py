b6df0299caa96bacc015ef95d14e96db629582d4
Alexander VanTol <avantol13@gmail.com>
2018-10-05 16:15:17 -0500

    chore(formatting): blacken

--- fence/oidc/oidc_server.py
+++ fence/oidc/oidc_server.py
@@ -1,7 +1,7 @@
 from authlib.common.urls import urlparse, url_decode
 from authlib.flask.oauth2 import AuthorizationServer
 from authlib.specs.rfc6749.authenticate_client import (
-    ClientAuthentication as AuthlibClientAuthentication
+    ClientAuthentication as AuthlibClientAuthentication,
 )
 from authlib.specs.rfc6749.errors import InvalidClientError as AuthlibClientError
 import flask
