d65b32e5a6073dd108ba748ae51ac024842884e5
philloooo <phillis.tt@gmail.com>
2018-11-08 16:22:07 -0600

    chore(job): add google user sa job

---
+++ bin/google-user-sa-validation.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+update-ca-certificates
+mkdir /google_job
+touch /google_job/status.json
+echo '<virtualhost *:80>
+    documentroot /google_job/
+
+    aliasmatch ^/(.*)$ /google_job/status.json
+
+    <directory "/google_job/status.json">
+      require all granted
+    </directory>
+</virtualhost>' >/etc/apache2/sites-available/fence.conf
+rm -rf /var/run/apache2/apache2.pid
+/usr/sbin/apache2ctl start
+while [ $? -eq 0 ]; do
+    fence-create google-manage-user-registrations
+    echo "{'last_run': '$(date)'}" >/google_job/status.json
+done
+
