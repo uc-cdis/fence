e85e757fb1d0331bd9e9a703381d047af69a9d7e
Alexander VanTol <avantol13@gmail.com>
2018-11-13 09:50:42 -0600

    tests(users): case insensitive search by username in test

--- tests/dbgap_sync/test_user_sync.py
+++ tests/dbgap_sync/test_user_sync.py
@@ -35,32 +35,30 @@ def test_sync(syncer, db_session, storage_client):
     proj = db_session.query(models.Project).all()
     assert len(proj) == 9
 
-    user = db_session.query(models.User).filter_by(username="USERC").one()
+    user = models.query_for_user(session=db_session, username="USERC")
     assert user.project_access == {
         "phs000178": ["read-storage"],
         "TCGA-PCAWG": ["read-storage"],
         "phs000179.c1": ["read-storage"],
     }
 
-    user = db_session.query(models.User).filter_by(username="USERF").one()
+    user = models.query_for_user(session=db_session, username="USERF")
     assert user.project_access == {
         "phs000178.c1": ["read-storage"],
         "phs000178.c2": ["read-storage"],
     }
 
-    user = db_session.query(models.User).filter_by(username="TESTUSERB").one()
+    user = models.query_for_user(session=db_session, username="TESTUSERB")
     assert user.project_access == {
         "phs000179.c1": ["read-storage"],
         "phs000178.c1": ["read-storage"],
     }
 
-    user = db_session.query(models.User).filter_by(username="TESTUSERD").one()
+    user = models.query_for_user(session=db_session, username="TESTUSERD")
     assert user.display_name == "USER D"
     assert user.phone_number == "123-456-789"
 
-    user = (
-        db_session.query(models.User).filter_by(username="test_user1@gmail.com").one()
-    )
+    user = models.query_for_user(session=db_session, username="test_user1@gmail.com")
 
     user_access = db_session.query(models.AccessPrivilege).filter_by(user=user).all()
 
@@ -73,9 +71,8 @@ def test_sync(syncer, db_session, storage_client):
     }
     assert len(user_access) == 1
 
-    user = (
-        db_session.query(models.User).filter_by(username="deleted_user@gmail.com").one()
-    )
+    user = models.query_for_user(session=db_session, username="deleted_user@gmail.com")
+
     assert not user.is_admin
 
     user_access = db_session.query(models.AccessPrivilege).filter_by(user=user).all()
@@ -100,7 +97,7 @@ def test_sync_from_files(syncer, db_session, storage_client):
 
     syncer.sync_to_db_and_storage_backend(phsids, userinfo, sess)
 
-    u = sess.query(models.User).filter_by(username="userB").one()
+    u = models.query_for_user(session=db_session, username="userB")
     u.project_access["phs000179"].sort()
     assert u.project_access == {"phs000179": ["read-storage", "write-storage"]}
 
@@ -124,7 +121,7 @@ def test_sync_revoke(syncer, db_session, storage_client):
     syncer.sync_to_db_and_storage_backend(phsids, userinfo, db_session)
     syncer.sync_to_db_and_storage_backend(phsids2, userinfo, db_session)
 
-    user_B = db_session.query(models.User).filter_by(username="userB").first()
+    user_B = models.query_for_user(session=db_session, username="userB")
 
     n_access_privilege = (
         db_session.query(models.AccessPrivilege).filter_by(user_id=user_B.id).count()
@@ -324,18 +321,14 @@ def test_update_arborist(syncer, db_session):
     # For every user in the user data, check that the matching policies were
     # created, and also granted to this user, i.e. the entry in the database
     # for this user has policies for everything in the original user data.
-    for user, data in user_data["users"].items():
+    for username, data in user_data["users"].items():
         if "projects" not in data:
             continue
         for project in data["projects"]:
             for privilege in project["privilege"]:
                 policy_id = _format_policy_id(project["resource"], privilege)
                 assert policy_id in policy_ids
-                user_policies = (
-                    db_session.query(models.User)
-                    .filter_by(username=user)
-                    .first()
-                    .policies
-                )
+                user = models.query_for_user(session=db_session, username=username)
+                user_policies = user.policies
                 user_policy_ids = [policy.id for policy in user_policies]
                 assert policy_id in user_policy_ids
