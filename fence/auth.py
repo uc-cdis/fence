a111fa47e5ab2666a903b5f227ad24d83d50887d
Jesus Pedrosa <jesuspg@uchicago.edu>
2018-12-03 22:52:35 +0000

    feat(awg-login): fix app typo on redirection

--- fence/auth.py
+++ fence/auth.py
@@ -55,7 +55,7 @@ def login_user(request, username, provider):
             if request_next.startswith('https') or request_next.startswith('http'):
                 next_url = request_next
             else:
-                next_url = build_redirect_url(app.config.get('ROOT_URL', ''), request_next)
+                next_url = build_redirect_url(flask.current_app.config.get('ROOT_URL', ''), request_next)
     	    redirect_response = flask.make_response(flask.redirect(next_url+"?error=401"))
 	    return redirect_response 
     flask.g.user = user
