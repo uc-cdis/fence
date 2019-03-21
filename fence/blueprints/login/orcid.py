ba3ed4ba4043ea70fa0fd650c291b8fcef4215be
Alexander VanTol <avantol13@gmail.com>
2019-03-21 14:48:21 -0500

    fix(login): use correct name for orcid idp

--- fence/blueprints/login/orcid.py
+++ fence/blueprints/login/orcid.py
@@ -8,7 +8,7 @@ from fence.blueprints.login._default import DefaultLogin, DefaultCallback
 class ORCIDLogin(DefaultLogin):
     def __init__(self):
         super(ORCIDLogin, self).__init__(
-            idp_name=IdentityProvider.microsoft,
+            idp_name=IdentityProvider.orcid,
             client=flask.current_app.orcid_client,
             mock_username="0000-0002-2601-8132",
         )
@@ -17,5 +17,5 @@ class ORCIDLogin(DefaultLogin):
 class ORCIDCallback(DefaultCallback):
     def __init__(self):
         super(ORCIDCallback, self).__init__(
-            idp_name=IdentityProvider.microsoft, client=flask.current_app.orcid_client
+            idp_name=IdentityProvider.orcid, client=flask.current_app.orcid_client
         )
