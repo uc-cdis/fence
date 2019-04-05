d6cb214a180c90d9c32872fc25d5c043d74d218c
Alexander VanTol <avantol13@gmail.com>
2019-04-05 15:40:26 -0500

    feat(linking): control /link/google endpoints with config for now, add support for future use of scope/aud

--- fence/jwt/token.py
+++ fence/jwt/token.py
@@ -16,7 +16,8 @@ SCOPE_DESCRIPTION = {
     "user": "Know your {idp_names} basic account information and what you are authorized to access.",
     "data": "Retrieve controlled-access datasets to which you have access on your behalf.",
     "credentials": "View and update your credentials.",
-    "google_credentials": "Receive temporary Google credentials to access data on google",
+    "google_link": "Allow providing your personal Google account access to data on Google.",
+    "google_credentials": "Receive temporary Google credentials to access data on Google.",
     "google_service_account": "Allow registration of external Google service accounts to access data.",
     "admin": "View and update user authorizations.",
 }
@@ -505,4 +506,3 @@ def generate_id_token(
     token.validate()
 
     return token
-
