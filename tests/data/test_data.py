8e656d6677b6664fadb25816a25d50f4a48a12a4
Rudyard Richter <rudyardrichter@uchicago.edu>
2019-04-11 14:29:47 -0500

    feat(download-auth): add test for download endpoint using rbac

--- tests/data/test_data.py
+++ tests/data/test_data.py
@@ -33,7 +33,6 @@ def test_indexd_download_file(
     Test ``GET /data/download/1``.
     """
     indexed_file_location = indexd_client["indexed_file_location"]
-
     path = "/data/download/1"
     query_string = {"protocol": indexed_file_location}
     headers = {
@@ -551,3 +550,52 @@ def test_blank_index_upload_unauthorized(
         response = client.post("/data/upload", headers=headers, data=data)
         data_requests.post.assert_not_called()
         assert response.status_code == 403, response
+
+
+@pytest.mark.parametrize(
+    "indexd_client_with_arborist", ["gs", "s3", "gs_acl", "s3_acl", "s3_external"], indirect=True
+)
+def test_rbac(
+    app,
+    client,
+    mock_arborist_requests,
+    indexd_client_with_arborist,
+    user_client,
+    rsa_private_key,
+    kid,
+    google_proxy_group,
+    primary_google_service_account,
+    cloud_manager,
+    google_signed_url,
+):
+    mock_arborist_requests({
+        "arborist/auth/request": {
+            "POST": ('{"auth": "true"}', 200),
+        },
+    })
+    indexd_client = indexd_client_with_arborist("test_rbac")
+    indexed_file_location = indexd_client["indexed_file_location"]
+    path = "/data/download/1"
+    query_string = {"protocol": indexed_file_location}
+    headers = {
+        "Authorization": "Bearer "
+        + jwt.encode(
+            utils.authorized_download_context_claims(
+                user_client.username, user_client.user_id
+            ),
+            key=rsa_private_key,
+            headers={"kid": kid},
+            algorithm="RS256",
+        )
+    }
+    response = client.get(path, headers=headers, query_string=query_string)
+    assert response.status_code == 200
+    assert "url" in response.json.keys()
+
+    mock_arborist_requests({
+        "arborist/auth/request": {
+            "POST": ('{"auth": "false"}', 403),
+        },
+    })
+    response = client.get(path, headers=headers, query_string=query_string)
+    assert response.status_code == 403
