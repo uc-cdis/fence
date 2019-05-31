01b8c4c2794734f15e658e77b3c896fa1a44e71e
Yajing Tang <phillis.tt@gmail.com>
2019-05-31 09:20:48 -0500

    chore(oauth2): allow specifying idp (#635)
    * chore(oauth2): allow specifying idp
    
    * chore(oauth): update swagger

--- fence/blueprints/login/__init__.py
+++ fence/blueprints/login/__init__.py
@@ -18,6 +18,18 @@ from fence.restful import RestfulApi
 from fence.config import config
 
 
+logger = get_logger(__name__)
+
+# Mapping from IDP ID to the name in the URL on the blueprint (see below).
+IDP_URL_MAP = {
+    "fence": "fence",
+    "google": "google",
+    "shibboleth": "shib",
+    "orcid": "orcid",
+    "microsoft": "microsoft",
+}
+
+
 def make_login_blueprint(app):
     """
     Args:
@@ -40,15 +52,6 @@ def make_login_blueprint(app):
         )
         default_idp = None
         idps = {}
-
-    # Mapping from IDP ID to the name in the URL on the blueprint (see below).
-    IDP_URL_MAP = {
-        "fence": "fence",
-        "google": "google",
-        "shibboleth": "shib",
-        "orcid": "orcid",
-        "microsoft": "microsoft",
-    }
 
     # check if google is configured as a client. we will at least need a
     # a callback if it is
