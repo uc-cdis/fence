cbf8e5b384bd7d7f781439f75904e6d7cacf5e38
Alexander VanTol <avantol13@gmail.com>
2019-06-03 09:26:13 -0500

    chore(sync): add a log when no path given for user.yaml

--- fence/sync/sync_users.py
+++ fence/sync/sync_users.py
@@ -134,6 +134,9 @@ class UserYAML(object):
                 file_contents = f.read()
                 validate_user_yaml(file_contents)  # run user.yaml validation tests
                 data = yaml.safe_load(file_contents)
+        else:
+            self.logger.info("Did not sync a user.yaml, no file path provided.")
+
         projects = dict()
         user_info = dict()
         policies = dict()
