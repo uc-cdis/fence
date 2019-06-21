7683ebc3313f3e1d13bc49a09de2f40695595b23
Alexander VanTol <avantol13@gmail.com>
2019-06-21 12:52:46 -0500

    chore(sync): update docstrings with example for util combine func

--- fence/sync/utils.py
+++ fence/sync/utils.py
@@ -3,6 +3,115 @@ def combine_provided_and_dbgap_resources(useryaml_resources, arborist_paths):
     Combine provided user.yaml resources loaded into python list of dictionaries
     and a list of string that are arborist resource paths for dbgap.
 
+    useryaml_resources should be read from user.yaml file into list of python dicts.
+    The output should also be in that format, but should convert a list of
+    arborist resources paths and combine that with resources from the user.yaml
+
+    Example input:
+
+        useryaml_resources: [
+            {"name": "gen3", "subresources": [{"name": "programs"}]},
+            {
+                "name": "programs",
+                "subresources": [
+                    {
+                        "name": "QA",
+                        "subresources": [
+                            {"name": "projects", "subresources": [{"name": "test"}]}
+                        ],
+                    },
+                    {
+                        "name": "DEV",
+                        "subresources": [
+                            {"name": "projects", "subresources": [{"name": "test"}]}
+                        ],
+                    },
+                    {
+                        "name": "phs000172",
+                        "subresources": [
+                            {"name": "projects", "subresources": [{"name": "test"}]}
+                        ],
+                    },
+                ],
+            },
+        ]
+
+        arborist_paths: [
+            "/programs/phs000172",
+            "/orgA/programs/phs000175",
+            "/orgC/programs/phs000175",
+            "/programs/phs000178",
+            "/orgA/programs/phs000179",
+            "/orgB/programs/phs000179",
+        ]
+
+        output: [
+            {
+                "name": "orgB",
+                "subresources": [
+                    {
+                        "name": "programs",
+                        "subresources": [{"name": "phs000179", "subresources": []}],
+                    }
+                ],
+            },
+            {
+                "name": "orgC",
+                "subresources": [
+                    {
+                        "name": "programs",
+                        "subresources": [{"name": "phs000175", "subresources": []}],
+                    }
+                ],
+            },
+            {
+                "name": "orgA",
+                "subresources": [
+                    {
+                        "name": "programs",
+                        "subresources": [
+                            {"name": "phs000179", "subresources": []},
+                            {"name": "phs000175", "subresources": []},
+                        ],
+                    }
+                ],
+            },
+            {"name": "gen3", "subresources": [{"name": "programs", "subresources": []}]},
+            {
+                "name": "programs",
+                "subresources": [
+                    {
+                        "name": "phs000172",
+                        "subresources": [
+                            {
+                                "name": "projects",
+                                "subresources": [{"name": "test", "subresources": []}],
+                            }
+                        ],
+                    },
+                    {
+                        "name": "QA",
+                        "subresources": [
+                            {
+                                "name": "projects",
+                                "subresources": [{"name": "test", "subresources": []}],
+                            }
+                        ],
+                    },
+                    {"name": "phs000178", "subresources": []},
+                    {
+                        "name": "DEV",
+                        "subresources": [
+                            {
+                                "name": "projects",
+                                "subresources": [{"name": "test", "subresources": []}],
+                            }
+                        ],
+                    },
+                ],
+            },
+        ]
+
     Args:
         useryaml_resources (list(dict)): Description
         arborist_paths (list(str)): Description
