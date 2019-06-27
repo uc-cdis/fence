7bdc7fe8a81af67cccb3eca84a7406d879655c95
Rudyard Richter <rudyardrichter@uchicago.edu>
2019-06-27 10:47:17 -0500

    feat(privacy-policy): allow config redirect to policy

--- fence/blueprints/privacy/blueprint.py
+++ fence/blueprints/privacy/blueprint.py
@@ -3,6 +3,8 @@ import pkgutil
 import flask
 from markdown import Markdown
 
+from fence import config
+
 blueprint = flask.Blueprint("privacy-policy", __name__)
 
 
@@ -12,6 +14,9 @@ PRIVACY_POLICY_HTML = Markdown().convert(PRIVACY_POLICY_MD)
 
 @blueprint.route("/", methods=["GET"])
 def privacy_policy():
+    # Check if we want to redirect out for the privacy policy.
+    if config.get("PRIVACY_POLICY"):
+        return flask.redirect(config["PRIVACY_POLICY"])
     if "text/markdown" in str(flask.request.accept_mimetypes).lower():
         return flask.Response(PRIVACY_POLICY_MD, mimetype="text/markdown")
     else:
