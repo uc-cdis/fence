de85db06e0623173c7221783f529f552f1714004
Alexander VanTol <avantol13@gmail.com>
2019-06-28 09:31:10 -0500

    chore(inline): inline function only called one place

--- fence/blueprints/data/indexd.py
+++ fence/blueprints/data/indexd.py
@@ -367,7 +367,7 @@ class IndexedFile(object):
 
     @cached_property
     def public(self):
-        return check_public(self.authz)
+        return "*" in self.authz or "/open" in self.authz
 
     @login_required({"data"})
     def check_authorization(self, action):
@@ -829,8 +829,3 @@ def filter_auth_ids(action, list_auth_ids):
         if checked_permission in values:
             authorized_dbgaps.append(key)
     return authorized_dbgaps
-
-
-def check_public(authz):
-    if "*" in authz or "/open" in authz:
-        return True
