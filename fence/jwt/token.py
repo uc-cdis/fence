d976c8324ff7f006abfb69388092cf656e2ca5e2
Alexander VanTol <avantol13@gmail.com>
2019-09-03 12:37:07 -0500

    feat(tokens): make sure "projects" still exists in the token, truncate access if too large

--- fence/jwt/token.py
+++ fence/jwt/token.py
@@ -367,6 +367,7 @@ def generate_signed_access_token(
                 "name": user.username,
                 "is_admin": user.is_admin,
                 "google": {"proxy_group": user.google_proxy_group_id},
+                "projects": {},
             }
         },
         "azp": client_id or "",
@@ -381,6 +382,11 @@ def generate_signed_access_token(
     if len(dict(user.project_access)) < config["TOKEN_PROJECTS_CUTOFF"]:
         claims["context"]["user"]["projects"] = dict(user.project_access)
     else:
+        # truncate to configured number of projects in token
+        claims["context"]["user"]["projects"] = {
+            k: dict(user.project_access)[k]
+            for k in list(dict(user.project_access))[: config["TOKEN_PROJECTS_CUTOFF"]]
+        }
         logger.warning(
             "NOT including project_access = {} in claims for user {} because there are too many projects for the token\n".format(
                 user.project_access, user.username
