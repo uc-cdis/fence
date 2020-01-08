faf6e431dd3813c3eea505fea020458f02874497
Michael <mjmartinson@uchicago.edu>
2020-01-08 15:21:07 -0600

    reformatted with black

--- fence/blueprints/data/indexd.py
+++ fence/blueprints/data/indexd.py
@@ -618,12 +618,14 @@ class S3IndexedFileLocation(IndexedFileLocation):
 
         region = self.get_bucket_region()
         if not region:
-            raise ValueError("No region specified. The region field is required for S3_BUCKETS")
+            raise ValueError(
+                "No region specified. The region field is required for S3_BUCKETS"
+            )
             # region = flask.current_app.boto.get_bucket_region(
             #     self.parsed_url.netloc, credential
             # )
-            # This requests takes very long so to improve efficiency region is required for s3 buckets.
-            # calling get_bucket_region increases resonse time by 50%
+            # This request takes very long, so to improve efficiency region is required for s3 buckets
+            # Calling get_bucket_region increases resonse time by 50%
 
         user_info = _get_user_info()
 
@@ -681,12 +683,14 @@ class S3IndexedFileLocation(IndexedFileLocation):
 
         region = self.get_bucket_region()
         if not region:
-            raise ValueError("No region specified. The region field is required for S3_BUCKETS")
+            raise ValueError(
+                "No region specified. The region field is required for S3_BUCKETS"
+            )
             # region = flask.current_app.boto.get_bucket_region(
             #     self.parsed_url.netloc, credential
             # )
-            # This requests takes very long so to improve efficiency region is required for s3 buckets
-            # calling get_bucket_region increases resonse time by 50%
+            # This request takes very long, so to improve efficiency region is required for s3 buckets
+            # Calling get_bucket_region increases resonse time by 50%
 
         return multipart_upload.generate_presigned_url_for_uploading_part(
             self.parsed_url.netloc,
