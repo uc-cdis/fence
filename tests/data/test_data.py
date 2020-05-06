41c215dfafcb28d99c5b6947138de5d807dbaf13
Zakir Gowani <zakirgowani@gmail.com>
2020-05-06 14:05:27 -0500

    commit

--- tests/data/test_data.py
+++ tests/data/test_data.py
@@ -670,8 +670,6 @@ def test_delete_file_no_auth(app, client, encoded_creds_jwt):
         "did": did,
         "baseid": "",
         "rev": "",
-        # uploader field is no longer sufficient for auth:
-        "uploader": user_client.username,
         "size": 10,
         "file_name": "file1",
         "urls": ["s3://bucket1/key-{}".format(did[:8])],
