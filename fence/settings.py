c56064c582ee904946ae4c0ee2cc4c04b58d3335
Michael Martinson <mjmartinson@uchicago.edu>
2020-05-22 11:12:26 -0700

    empty

--- fence/settings.py
+++ fence/settings.py
@@ -5,7 +5,7 @@ NOTE TO DEVELOPERS: This is NOT intended to be a place for configurable variable
                     Please put new cfg into the config-default.yaml
 
 This exists primarily as a legacy support for old configuration, but has been
-repurposed to hold some info used when searching for a provided configuration variable.
+repurposed to hold some info used when searching for a provided configuration variable. 
 """
 from os.path import expanduser
 
