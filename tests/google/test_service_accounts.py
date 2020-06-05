537d0eca8f9687cd6ca77353d80cdf7c7df8b29f
Pauline Ribeyre <ribeyre@uchicago.edu>
2020-06-05 15:56:58 -0500

    fix "assert 60 <= 59" error in flaky unit tests

--- tests/google/test_service_accounts.py
+++ tests/google/test_service_accounts.py
@@ -222,7 +222,7 @@ def test_patch_service_account_expires_in(
     ).all()
     for access in service_account_accesses:
         diff = access.expires - int(time.time())
-        assert requested_exp <= diff <= requested_exp + 10
+        assert requested_exp - 2 <= diff <= requested_exp + 10
 
 
 def test_patch_service_account_dry_run_valid_empty_arg(
@@ -808,7 +808,7 @@ def test_service_account_registration_expires_in(
     # make sure the access was granted for the requested time
     # (allow up to 10 sec for runtime)
     diff = sa_to_bucket_entries[0].expires - int(time.time())
-    assert requested_exp <= diff <= requested_exp + 10
+    assert requested_exp - 2 <= diff <= requested_exp + 10
 
     # invalid expires_in: should fail
     requested_exp = "abc"  # expires_in must be int >0
