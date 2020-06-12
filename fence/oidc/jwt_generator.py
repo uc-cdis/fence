e0e920942072ded8c27bb211a1ee2bd4dd0bc38b
vpsx <19900057+vpsx@users.noreply.github.com>
2020-06-12 17:21:15 -0500

    feat(at-hash): Include at_hash in id tokens in code flow

--- fence/oidc/jwt_generator.py
+++ fence/oidc/jwt_generator.py
@@ -195,6 +195,8 @@ def generate_token_response(
         nonce=nonce,
         linked_google_email=linked_google_email,
         linked_google_account_exp=linked_google_account_exp,
+        auth_flow_type=AuthFlowTypes.CODE,
+        access_token=access_token,
     ).token
     # If ``refresh_token`` was passed (for instance from the refresh
     # grant), use that instead of generating a new one.
