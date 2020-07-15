d2df60ce65d79e5c60d7a9d60de91f28e6a13cac
vpsx <19900057+vpsx@users.noreply.github.com>
2020-07-15 16:21:34 -0500

    feat(ga4gh-scope): Check access tkn scopes in userinfo

--- fence/resources/user/__init__.py
+++ fence/resources/user/__init__.py
@@ -130,9 +130,15 @@ def get_user_info(current_session, username):
         info.update(optional_info)
 
     # Include ga4gh passport visas
-    # TODO: Respect access token claims (only include if ga4gh_passport_v1 scope present)
-    encoded_visas = [row.ga4gh_visa for row in user.ga4gh_visas_v1]
-    info["ga4gh_passport_v1"] = encoded_visas
+    if not flask.g.access_token:
+        logger.warning(
+            "Session token present but no access token found. Unable to check scopes in userinfo; returning."
+        )
+    else:
+        at_scopes = jwt.decode(flask.g.access_token, verify=False).get("scope", "")
+        if "ga4gh_passport_v1" in at_scopes:
+            encoded_visas = [row.ga4gh_visa for row in user.ga4gh_visas_v1]
+            info["ga4gh_passport_v1"] = encoded_visas
 
     return info
 
