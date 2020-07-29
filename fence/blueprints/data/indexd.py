6ad01d3bcd8436319dc62bd0109acbb26519373e
Zakir Gowani <zakirgowani@gmail.com>
2020-07-29 12:29:04 -0500

    url encode filenames for gcs

--- fence/blueprints/data/indexd.py
+++ fence/blueprints/data/indexd.py
@@ -887,7 +887,7 @@ class GoogleStorageIndexedFileLocation(IndexedFileLocation):
             )
             # GCS Filenames must be encoded according to https://tools.ietf.org/html/rfc3986#section-3.3
             # As described in this doc: https://cloud.google.com/storage/docs/request-endpoints
-            file_name = urlquote("/".join(resource_path.split("/")[1:]))
+            file_name = urlquote("/".join(resource_path.split("/")[1:]), safe=''))
         except Exception as exc:
             logger.error("Unable to get file name from resource path. {}".format(exc))
 
