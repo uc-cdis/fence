b158b3736a9f70a399b962f28bd51d2e432bf019
Marcelo Costa <marceloc@uchicago.edu>
2020-07-31 18:32:41 -0500

    inject nginx config in the correct file

--- dockerrun.bash
+++ dockerrun.bash
@@ -21,11 +21,10 @@ fi
 
 # add nginx status config
 nginx_status_conf="\ \ \ \ location /nginx_status {\n\ \ \ \ \ \ stub_status;\n\ \ \ \ \ \ allow 127.0.0.1;\n\ \ \ \ \ \ deny all;\n\ \ \ \ \ \ access_log off;\n\ \ \ \ }"
-sed -i "/\ \ \ \ error_page\ 502/i ${nginx_status_conf}" /etc/nginx/conf.d/uwsgi.conf
-
+sed -i "/\ \ \ \ error_page\ 502/i ${nginx_status_conf}" /etc/nginx/sites-available/uwsgi.conf
 # add uwsgi status config
 uwsgi_status_conf="\ \ \ \ location /uwsgi_status {\n\ \ \ \ \ \ proxy_pass \"http://127.0.0.1:9191\";\n\ \ \ \ \ \ allow 127.0.0.1;\n\ \ \ \ \ \ deny all;\n\ \ \ \ \ \ access_log off;\n\ \ \ \ }"
-sed -i "/\ \ \ \ error_page\ 502/i ${uwsgi_status_conf}" /etc/nginx/conf.d/uwsgi.conf
+sed -i "/\ \ \ \ error_page\ 502/i ${uwsgi_status_conf}" /etc/nginx/sites-available/uwsgi.conf
 
 # add another access log in a non-json format
 additional_access_log_conf="\ \ \ \ access_log  /var/log/nginx/access_not_json.log main;"
