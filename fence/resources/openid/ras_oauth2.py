399086c0f38a9cf0b49c0467e8a3228804b6298a
BinamB <numb.baj@gmail.com>
2020-09-14 14:56:37 -0500

    try userinfo

--- fence/resources/openid/ras_oauth2.py
+++ fence/resources/openid/ras_oauth2.py
@@ -61,7 +61,11 @@ class RASOauth2Client(Oauth2ClientBase):
 
             token = self.get_token(token_endpoint, code)
             keys = self.get_jwt_keys(jwks_endpoint)
-            userinfo = self.get_userinfo(token, userinfo_endpoint)
+            userinfo = {}
+            try:
+                userinfo = self.get_userinfo(token, userinfo_endpoint)
+            except:
+                pass
 
             claims = jose_jwt.decode(
                 token["id_token"],
