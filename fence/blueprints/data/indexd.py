5d70405ed9aeffc7ec9ef09b19bbc9a1e4f0ff8e
vpsx <19900057+vpsx@users.noreply.github.com>
2020-09-21 17:25:40 -0500

    fix(aud-scope): Fix validate_request argument

--- fence/blueprints/data/indexd.py
+++ fence/blueprints/data/indexd.py
@@ -1147,7 +1147,7 @@ def _get_user_info(sub_to_string=True):
     populated information about an anonymous user.
     """
     try:
-        set_current_token(validate_request(aud="user"))
+        set_current_token(validate_request(scope={"user"}))
         user_id = current_token["sub"]
         if sub_to_string:
             user_id = str(user_id)
