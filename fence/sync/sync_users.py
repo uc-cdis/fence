01495d780ccba9b42d85520720faa83679c7af46
mattgarvin1 <mattgarvin1@gmail.com>
2020-10-19 15:36:11 -0500

    test exception handling

--- fence/sync/sync_users.py
+++ fence/sync/sync_users.py
@@ -1388,7 +1388,10 @@ class UserSyncer(object):
                 response = self.arborist_client.update_role(role["id"], role)
                 if response:
                     self._created_roles.add(role["id"])
-            except:
+            except Exception as e:
+                # debug / dev'ing - expecting this to be an ArboristError
+                print("\tHERE - arborist error:\n")
+                print("\t", e)
                 try:
                     response = self.arborist_client.create_role(role)
                     if response:
