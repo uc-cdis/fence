39e0423a050f8f2e1ade2c195f14c2d4a3cd78e9
BinamB <numb.baj@gmail.com>
2021-01-13 09:07:26 -0600

    draft flow

--- fence/job/visa_update_cronjob.py
+++ fence/job/visa_update_cronjob.py
@@ -20,11 +20,36 @@ class Visa_Token_Update(object):
         self.concurrency = concurrency or 3
         self.thread_pool_size = thread_pool_size or 2
         self.buffer_size = buffer_size or 10
+        self.n_workers = self.thread_pool_size + self.concurrency
         
-    def update_tokens(self):
+    async def update_tokens(self):
         """
         Have dictionary or something to decide which client to use. Can go through the whole list and decide which client to use 
         looking at the type field in the ga4gh table. 
+        """
+        
+        queue = asyncio.Queue(maxsize=self.buffer_size)
+
+        # Iterate through all users in upstream_refresh_token table (or should it be the ga4gh_visa_v1 table since we want to maximize access)
+            # No case where there might be a refresh token but no visa so should iterate through the visa table
+
+            # 
+
+
+        # Check what type of visa they have 
+
+        # Depending on the type of visa change their OauthClient 
+
+        
+
+    async def producer(self, queue):
+        """
+        TODO: Rename this
+        Producer: Produces users and puts them in a queue for processing
 
         """
-        client = RASClient()
+
+    async def consumer(self, queue):
+        """
+        TODO: Rename this
+        Consumer: Create workers that does the visa update flow 
