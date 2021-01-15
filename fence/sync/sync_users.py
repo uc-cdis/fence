e5e428f69319f2bc78a9c5563bed2092d9c05ca9
BinamB <numb.baj@gmail.com>
2021-01-15 12:17:49 -0600

    prints

--- fence/sync/sync_users.py
+++ fence/sync/sync_users.py
@@ -1502,6 +1502,14 @@ class UserSyncer(object):
         # from authorization sources get policies revoked
         arborist_user_projects = {}
         try:
+            print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+            print(self.arborist_client._user_url)
+            print(self.arborist_client.get_users(
+                url=self.arborist_client._user_url
+            ))
+            print(self.arborist_client.get_users(
+                url=self.arborist_client._user_url
+            ).json["users"])
             arborist_users = self.arborist_client.get_users(
                 url=self.arborist_client._user_url
             ).json["users"]
