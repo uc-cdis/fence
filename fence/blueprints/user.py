e1c5afa84856c3797d245270221f3880f93c4649
grugna <graglia01@gmail.com>
2021-01-20 10:33:34 -0600

    fix

--- fence/blueprints/user.py
+++ fence/blueprints/user.py
@@ -41,7 +41,7 @@ def update_user_info():
         additional_info["lastName"] = lastName
     if institution:
         additional_info["institution"] = institution
-        
+
     return flask.jsonify(update_user(current_session, additional_info))
 
 
