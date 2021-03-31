4422521467992a3c3c150ec164204ab0479addbe
vpsx <19900057+vpsx@users.noreply.github.com>
2021-03-31 09:10:00 -0500

    docs(register): Edit docstrings

--- fence/blueprints/register.py
+++ fence/blueprints/register.py
@@ -1,32 +1,7 @@
 """
 Endpoints for user registration.
-
-register_user:
-  - If config["REGISTER_USERS_ON"] is True, then unregistered users are redirected
-    here after logging in.
-  - Users may then register or decline to register.
-  - Users may access this endpoint directly if they initially declined to register.
-  - At the moment, registration involves providing name, org, and (if user.email for
-    the user is None) email.
-  - If a user registers, add the new information to the user's additional_info column,
-    and add the user to the Arborist group specified in
-    config["REGISTERED_USERS_GROUP"].
-    The idea is that users can register in order to obtain certain permissions (where
-    the permissions are defined by the group definition in the useryaml).
-
-  | The registration info is added as a dict under user.additional_info["registration_info"];
-  | it is a separate blob in order to avoid namespace collision and make clear that the
-  | information was self-declared by the user during registration.
-
-  | The HTML form performs some dumb client-side validation, but actual verification
-  | (for example, checking organization info against some trusted authority's records)
-  | has been deemed out of scope.
-
-get_registered_users:
-  - List registration info for every user for which there exists registration info.
-  - Endpoint accessible to admins only.
-  - Response json structure is provisional.
-
+Registration means that a user provides their name, org, and email,
+in order to gain some predefined permissions.
 """
 
 import flask
@@ -42,6 +17,27 @@ blueprint = flask.Blueprint("register-user", __name__)
 @blueprint.route("/", methods=["GET", "POST"])
 @login_required()
 def register_user():
+    """
+    - If config["REGISTER_USERS_ON"] is True, then unregistered users are redirected
+      here after logging in.
+    - Users may then register or decline to register.
+    - Users may access this endpoint directly if they initially declined to register.
+    - At the moment, registration involves providing name, org, and (if user.email for
+      the user is None) email.
+    - If a user registers, add the new information to the user's additional_info column,
+      and add the user to the Arborist group specified in
+      config["REGISTERED_USERS_GROUP"].
+      The idea is that users can register in order to obtain certain permissions (where
+      the permissions are defined by the group definition in the useryaml).
+
+    The registration info is added as a dict under user.additional_info["registration_info"];
+    it is a separate blob in order to avoid namespace collision and make clear that the
+    information was self-declared by the user during registration.
+
+    The HTML form performs some dumb client-side validation, but actual verification
+    (for example, checking organization info against some trusted authority's records)
+    has been deemed out of scope.
+    """
     if flask.request.method == "GET":
         on_decline_redirect = flask.session.get("redirect") or config["BASE_URL"]
         return flask.render_template(
@@ -91,6 +87,11 @@ def register_user():
 @blueprint.route("/list", methods=["GET"])
 @admin_login_required
 def get_registered_users():
+    """
+    - List registration info for every user for which there exists registration info.
+    - Endpoint accessible to admins only.
+    - Response json structure is provisional.
+    """
     registered_users = (
         current_session.query(User)
         .filter(User.additional_info["registration_info"] != "{}")
