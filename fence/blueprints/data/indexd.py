f642aab56ea5df81e6e9e83030129ca54e39cd2c
Michael Lukowski <lukowski9@gmail.com>
2021-05-10 12:41:56 -0500

    fix psql insert

--- fence/blueprints/data/indexd.py
+++ fence/blueprints/data/indexd.py
@@ -1074,11 +1074,13 @@ class GoogleStorageIndexedFileLocation(IndexedFileLocation):
                             gcp_proxy_group_id = EXCLUDED.gcp_proxy_group_id,
                             gcp_private_key = EXCLUDED.gcp_private_key,
                             gcp_key_db_entry = EXCLUDED.gcp_key_db_entry;""",
-                        dict(
-                            gcp_proxy_group_id=proxy_group_id,
-                            gcp_private_key=private_key,
-                            gcp_key_db_entry=key_db_entry,
-                            expires_at=expiration_time,
+                        json.dumps(
+                            dict(
+                                gcp_proxy_group_id=proxy_group_id,
+                                gcp_private_key=private_key,
+                                gcp_key_db_entry=key_db_entry,
+                                expires_at=expiration_time,
+                            )
                         ),
                     )
 
