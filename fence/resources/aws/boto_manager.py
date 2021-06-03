df04f49b03325aad873aafdf39cac6e7cbe628eb
Sai Shanmukha <nss10@outlook.com>
2021-06-03 14:32:57 -0500

    Add tests for aws_assume_role_time_limit issue

--- fence/resources/aws/boto_manager.py
+++ fence/resources/aws/boto_manager.py
@@ -13,6 +13,7 @@ class BotoManager(object):
 
     URL_EXPIRATION_DEFAULT = 1800  # 30 minutes
     URL_EXPIRATION_MAX = 86400  # 1 day
+    AWS_ASSUME_ROLE_MIN_LIMIT = 900  # 15 minutes
 
     def __init__(self, config, logger):
         self.sts_client = client("sts", **config)
@@ -67,7 +68,7 @@ class BotoManager(object):
                 self.sts_client = client("sts", **config)
             session_name_postfix = uuid.uuid4()
             duration_seconds = max(
-                900, duration_seconds
+                self.AWS_ASSUME_ROLE_MIN_LIMIT, duration_seconds
             )  # Since minimum time for aws assume role is 900 seconds as per boto docs
             return self.sts_client.assume_role(
                 RoleArn=role_arn,
