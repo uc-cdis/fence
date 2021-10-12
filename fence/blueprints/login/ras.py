7e13de95425984ff6a1923fa5efa9cf4b25ece82
BinamB <numb.baj@gmail.com>
2021-10-12 13:05:54 -0500

    remove stuff

--- fence/blueprints/login/ras.py
+++ fence/blueprints/login/ras.py
@@ -63,24 +63,19 @@ class RASCallback(DefaultOAuth2Callback):
             raise
 
         for encoded_visa in encoded_visas:
-            if flask.current_app.pkey_cache:
-                logger.info("Retrieving public key from flask app ...")
-                public_key = flask.current_app.pkey_cache
-            else:
-                logger.info("Failed to retrieve public key from flask app")
-                try:
-                    # Do not move out of loop unless we can assume every visa has same issuer and kid
-                    public_key = get_public_key_for_token(
-                        encoded_visa, attempt_refresh=True
+            try:
+                # Do not move out of loop unless we can assume every visa has same issuer and kid
+                public_key = get_public_key_for_token(
+                    encoded_visa, attempt_refresh=True
+                )
+            except Exception as e:
+                # (But don't log the visa contents!)
+                logger.error(
+                    "Could not get public key to validate visa: {}. Discarding visa.".format(
+                        e
                     )
-                except Exception as e:
-                    # (But don't log the visa contents!)
-                    logger.error(
-                        "Could not get public key to validate visa: {}. Discarding visa.".format(
-                            e
-                        )
-                    )
-                    continue
+                )
+                continue
 
             try:
                 # Validate the visa per GA4GH AAI "Embedded access token" format rules.
