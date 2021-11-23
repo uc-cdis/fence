820537e05b5c9aca4ce0efb478eddafb49e6885c
John McCann <johnmccann@uchicago.edu>
2021-11-23 11:41:45 -0800

    chore(grant_user_policy): supply expires_at

--- fence/sync/sync_users.py
+++ fence/sync/sync_users.py
@@ -5,6 +5,7 @@ import re
 import subprocess as sp
 import yaml
 import copy
+import datetime
 
 from contextlib import contextmanager
 from collections import defaultdict
@@ -1612,7 +1613,8 @@ class UserSyncer(object):
         Args:
             user_projects (dict)
             user_yaml (UserYAML) optional, if there are policies for users in a user.yaml
-            expiration (datetime.datetime): expiration time
+            TODO update this
+            expires (datetime.datetime): expiration time
 
         Return:
             bool: success
@@ -1731,9 +1733,12 @@ class UserSyncer(object):
                                     "not creating policy in arborist; {}".format(str(e))
                                 )
                             self._created_policies.add(policy_id)
-                        self.arborist_client.grant_user_policy(username, policy_id)
-                        # TODO need to add expiration to this function in gen3authz
-                        # self.arborist_client.grant_user_policy(username, policy_id, expiration=expiration)
+
+                        self.arborist_client.grant_user_policy(
+                            username,
+                            policy_id,
+                            expires_at=datetime.datetime.fromtimestamp(expires),
+                        )
 
             # TODO As of 10-11-2021, there's no endpoint yet in Arborist to
             # support the creation of policies in bulk. When syncing RAS
