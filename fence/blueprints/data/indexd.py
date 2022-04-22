bfe1fc5a2fa08d8c4768b32c9f9b3a5ca7608b3b
Alexander VanTol <Avantol13@users.noreply.github.com>
2022-04-22 10:27:08 -0500

    Update indexd.py

--- fence/blueprints/data/indexd.py
+++ fence/blueprints/data/indexd.py
@@ -683,7 +683,7 @@ class S3IndexedFileLocation(IndexedFileLocation):
                     .first()
                 )
                 # Add 10 seconds of buffer to epoch time to allow for better use of cache. 
-                if cache and cache.expires_at and (cache.expires_at+10) >= expiry:
+                if cache and cache.expires_at and cache.expires_at >= expiry:
                     rv = dict(
                         aws_access_key_id=cache.aws_access_key_id,
                         aws_secret_access_key=cache.aws_secret_access_key,
