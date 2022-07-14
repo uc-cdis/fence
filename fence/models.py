699379e8bf330a0f14c068ef0584fa8507f6a228
grugna <graglia01@gmail.com>
2022-07-14 18:11:34 -0500

    update initial document config and db update

--- fence/models.py
+++ fence/models.py
@@ -1312,32 +1312,20 @@ def _remove_policy(driver, md):
 #TODO remove this and add in deployment process instead 
 def _add_documents(driver, md):
     with driver.session as session:
-        if config.get("INITIAL_PRIVACY_POLICY") and config.get("INITIAL_PRIVACY_POLICY_RAW"):
-            pp = config["INITIAL_PRIVACY_POLICY"]
-            pp_raw = config["INITIAL_PRIVACY_POLICY_RAW"]
-            session.execute(
-            """\
-INSERT INTO document (type, version, name, raw, formatted, required)
-VALUES ('privacy-policy', '1', 'Privacy Notice', '{}', '{}', 'true')
-ON CONFLICT (type, version)
-DO NOTHING;""".format(pp_raw, pp)
-        )
-            session.commit()
-
-    if config.get("INITIAL_TERM_CONDITION") and config.get("INITIAL_TERM_CONDITION_RAW"):
-        tc = config["INITIAL_TERM_CONDITION"]
-        tc_raw = config["INITIAL_TERM_CONDITION_RAW"]
-        session.execute(
-            """\
-INSERT INTO document (type, version, name, raw, formatted, required)
-VALUES ('terms-and-conditions', '1', 'Terms and Conditions', '{}', '{}', 'true')
-ON CONFLICT (type, version)
-DO NOTHING;""".format(tc_raw, tc)
-        )
-        session.commit()
-        
-        # session.execute("INSERT INTO document(type, version, name, raw, formatted, required) values ('privacy-policy', '1', 'Privacy Notice', 'https://github.com/chicagopcdc/Documents/blob/81d60130308b6961c38097b6686a21f8be729a2c/governance/privacy_policy/privacy_notice.md', 'https://github.com/chicagopcdc/Documents/blob/81d60130308b6961c38097b6686a21f8be729a2c/governance/privacy_policy/PCDC-Privacy-Notice.pdf', 'true'), ('terms-and-conditions', '1', 'Terms and Conditions', 'https://github.com/chicagopcdc/Documents/blob/fda4a7c914173e29d13ab6249ded7bc9adea5674/governance/terms_and_conditions/GEN3_portal/terms-and-conditions.md', 'https://github.com/chicagopcdc/Documents/blob/a5f4a87262f6597fc85d95b74c320e4fdf1e9097/governance/terms_and_conditions/GEN3_portal/Pediatric%20Cancer%20Data%20Commons%20-%20Terms%20and%20Conditions.pdf', 'true') ON CONFLICT (type, version) DO NOTHING;")
-        # session.commit()
+        if config.get("INITIAL_DOCUMENTS"):
+            docs = config["INITIAL_DOCUMENTS"]
+            if len(docs.keys()) > 0:
+                for doc_k, doc_v in docs.items():
+                    session.execute(
+                        """\
+                        INSERT INTO document (type, version, name, raw, formatted, required)
+                        VALUES ('{}', '{}', '{}', '{}', '{}', '{}')
+                        ON CONFLICT (type, version)
+                        DO NOTHING;""".format(doc_v["type"], doc_v["version"], doc_v["name"], doc_v["raw"], doc_v["formatted"], doc_v["required"])
+                    )
+                    session.commit()
+            else:
+                raise ValueError("The initial document are missing or the format is wrong: INITIAL_DOCUMENTS.")
 
 
 def _add_google_project_id(driver, md):
