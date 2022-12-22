28d437f95257bad475d981159add3f1002c37325
Andrew Prokhorenkov <aprokh@uchicago.edu>
2022-12-21 23:45:03 -0600

    feat: syncing groups to Arborist policies

--- fence/resources/cognito/groups.py
+++ fence/resources/cognito/groups.py
@@ -1,7 +1,9 @@
 from flask_sqlalchemy_session import current_session
 
+import fence.scripting.fence_create
 
-def sync_gen3_users_authz_from_adfs_groups(email, groups, db_session=None):
+
+def sync_gen3_users_authz_from_adfs_groups(email, groups):
     """
     Sync the authorization of users in the Gen3 database with the groups
     they are in on the ADFS server.
@@ -14,10 +16,26 @@ def sync_gen3_users_authz_from_adfs_groups(email, groups, db_session=None):
         dict: dictionary of users that were synced and the groups they were
             synced with
     """
-    db_session = db_session or current_session
-
     # for each group, assign current user the following resources:
     # /cohort-middleware/{group}
     # with both role_ids: 'cohort_middleware_admin' and 'cohort_middleware_outputs_admin_reader'
-    for group in groups:
-        pass
+    db_session = db_session or current_session
+    _sync_adfs_groups(
+        email,
+        groups,
+        db_session=db_session,
+    )
+
+
+def _sync_adfs_groups(gen3_user, groups, db_session=None):
+    db_session = db_session or current_session
+
+    default_args = fence.scripting.fence_create.get_default_init_syncer_inputs(
+        authz_provider="Cognito"
+    )
+    syncer = fence.scripting.fence_create.init_syncer(**default_args)
+
+    groups = syncer.sync_single_user_groups(
+        gen3_user,
+        groups,
+    )
