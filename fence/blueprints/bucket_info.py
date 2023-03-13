94c0ed0ffd8ee40da2c622ea9b956fe7b2881f1c
BinamB <numb.baj@gmail.com>
2023-03-13 13:32:58 -0500

    stuff

--- fence/blueprints/bucket_info.py
+++ fence/blueprints/bucket_info.py
@@ -1,20 +1,27 @@
 import flask
+
+from fence.auth import require_auth_header
 from fence.config import config
 
 blueprint = flask.Blueprint("bucket_info", __name__)
 
 
 @blueprint.route(
-    "/",
+    "/region",
     methods=["GET"],
 )
-def get_bucket_info():
-    S3_BUCKETS = config.get("S3_BUCKETS", {})
-    GS_BUCKETS = config.get("GS_BUCKETS", {})
+@require_auth_header(scope={"bucket_info"})
+def get_bucket_region_info():
+    S3_BUCKETS = config.get("S3_BUCKETS", {}).copy()
+    GS_BUCKETS = config.get("GS_BUCKETS", {}).copy()
     result = {"S3_BUCKETS": S3_BUCKETS, "GS_BUCKETS": GS_BUCKETS}
 
     for cloud, buckets in result.items():
         for bucket, info in buckets.items():
-            del info["cred"]
+            info_copy = info.copy()
+            for key in info.keys():
+                if key != "region":
+                    del info_copy[key]
+            buckets[bucket] = info_copy
 
     return flask.jsonify(result)
--- tests/test_bucket_info.py
+++ tests/test_bucket_info.py
@@ -2,15 +2,14 @@ import fence.blueprints.bucket_info
 
 
 def test_get_bucket_info(client):
-    res = client.get("/bucket_info")
+    res = client.get("/bucket_info/region")
 
-    print(res.json)
     assert res.status_code == 200
 
     bucket_info = res.json
     for cloud, buckets in bucket_info.items():
         for bucket, info in buckets.items():
-            assert "cred" not in bucket_info
+            assert "cred" not in info
 
     assert len(bucket_info["S3_BUCKETS"]) == 5
     assert len(bucket_info["GS_BUCKETS"]) == 2
