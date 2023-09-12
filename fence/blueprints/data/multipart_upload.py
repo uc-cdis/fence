0e2c5ec038748ff3b05b4c3be9f4113cac67ac79
Liam Beckman <lbeckman314@gmail.com>
2023-09-12 12:47:55 -0700

    Update support for multipart uploads to non-AWS S3 endpoints

--- fence/blueprints/data/multipart_upload.py
+++ fence/blueprints/data/multipart_upload.py
@@ -3,7 +3,9 @@ from botocore.exceptions import ClientError
 from retry.api import retry_call
 
 from cdispyutils.hmac4 import generate_aws_presigned_url
+from cdispyutils.config import get_value
 from cdislogging import get_logger
+from fence.config import config
 from fence.errors import InternalError
 
 MAX_TRIES = 5
@@ -11,7 +13,7 @@ MAX_TRIES = 5
 logger = get_logger(__name__)
 
 
-def initilize_multipart_upload(bucket, key, credentials):
+def initilize_multipart_upload(bucket_name, key, credentials):
     """
     Initialize multipart upload
 
@@ -23,17 +25,30 @@ def initilize_multipart_upload(bucket, key, credentials):
     Returns:
         UploadId(str): uploadId
     """
+    s3_buckets = get_value(
+        config, "S3_BUCKETS", InternalError("S3_BUCKETS not configured")
+    )
+    bucket = s3_buckets.get(bucket_name)
+
+    url = ""
+    if bucket.get("endpoint_url"):
+        url = bucket["endpoint_url"]
+
     session = boto3.Session(
         aws_access_key_id=credentials["aws_access_key_id"],
         aws_secret_access_key=credentials["aws_secret_access_key"],
         aws_session_token=credentials.get("aws_session_token"),
     )
-    s3client = session.client("s3")
+    s3client = None
+    if url:
+        s3client = session.client("s3", endpoint_url=url)
+    else:
+        s3client = session.client("s3")
 
     try:
         multipart_upload = retry_call(
             s3client.create_multipart_upload,
-            fkwargs={"Bucket": bucket, "Key": key},
+            fkwargs={"Bucket": bucket_name, "Key": key},
             tries=MAX_TRIES,
             jitter=10,
         )
@@ -48,7 +63,7 @@ def initilize_multipart_upload(bucket, key, credentials):
     return multipart_upload.get("UploadId")
 
 
-def complete_multipart_upload(bucket, key, credentials, uploadId, parts):
+def complete_multipart_upload(bucket_name, key, credentials, uploadId, parts):
     """
     Complete multipart upload.
     Raise exception if something wrong happens; otherwise success
@@ -64,18 +79,31 @@ def complete_multipart_upload(bucket, key, credentials, uploadId, parts):
     Return:
         None
     """
+    s3_buckets = get_value(
+        config, "S3_BUCKETS", InternalError("S3_BUCKETS not configured")
+    )
+    bucket = s3_buckets.get(bucket_name)
+
+    url = ""
+    if bucket.get("endpoint_url"):
+        url = bucket["endpoint_url"]
+
     session = boto3.Session(
         aws_access_key_id=credentials["aws_access_key_id"],
         aws_secret_access_key=credentials["aws_secret_access_key"],
         aws_session_token=credentials.get("aws_session_token"),
     )
-    s3client = session.client("s3")
+    s3client = None
+    if url:
+        s3client = session.client("s3", endpoint_url=url)
+    else:
+        s3client = session.client("s3")
 
     try:
         retry_call(
             s3client.complete_multipart_upload,
             fkwargs={
-                "Bucket": bucket,
+                "Bucket": bucket_name,
                 "Key": key,
                 "MultipartUpload": {"Parts": parts},
                 "UploadId": uploadId,
@@ -95,7 +123,7 @@ def complete_multipart_upload(bucket, key, credentials, uploadId, parts):
 
 
 def generate_presigned_url_for_uploading_part(
-    bucket, key, credentials, uploadId, partNumber, region, expires
+    bucket_name, key, credentials, uploadId, partNumber, region, expires
 ):
     """
     Generate presigned url for uploading object part given uploadId and part number
@@ -113,18 +141,24 @@ def generate_presigned_url_for_uploading_part(
         presigned_url(str)
     """
 
+    s3_buckets = get_value(
+        config, "S3_BUCKETS", InternalError("S3_BUCKETS not configured")
+    )
+    bucket = s3_buckets.get(bucket_name)
+
     if bucket.get("endpoint_url"):
         url = bucket["endpoint_url"].strip("/") + "/{}/{}".format(
-            bucket, key.strip("/")
+            bucket_name, key.strip("/")
         )
     else:
-        url = "https://{}.s3.amazonaws.com/{}".format(bucket, key)
+        url = "https://{}.s3.amazonaws.com/{}".format(bucket_name, key)
     additional_signed_qs = {"partNumber": str(partNumber), "uploadId": uploadId}
 
     try:
-        return generate_aws_presigned_url(
+        presigned_url = generate_aws_presigned_url(
             url, "PUT", credentials, "s3", region, expires, additional_signed_qs
         )
+        return presigned_url
     except Exception as e:
         raise InternalError(
             "Can not generate presigned url for part number {} of key {}. Detail {}".format(
