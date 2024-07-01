13dae1adbf8f155bcad40812775b407f309179c0
Ed <emalinowski@uchicago.edu>
2024-07-01 12:58:47 -0500

    chore(metrics): Added labels

--- fence/metrics.py
+++ fence/metrics.py
@@ -34,47 +34,47 @@ class Metrics:
             app.wsgi_app, {"/metrics": make_wsgi_app(registry=app.prometheus_registry)}
         )
         self.presigned_url_counter = Counter(
-            "fence_presigned_url_requests_total",
+            "gen3_fence_presigned_url_requests_total",
             "Total number of presigned URL requests",
             registry=app.prometheus_registry,
         )
         self.login_counter = Counter(
-            "fence_all_login_requests_total",
+            "gen3_fence_all_login_requests_total",
             "Total number of login requests",
             registry=app.prometheus_registry,
         )
         self.fence_login_counter = Counter(
-            "fence_login_requests_total",
+            "gen3_fence_login_requests_total",
             "Total number of fence login requests",
             registry=app.prometheus_registry,
         )
         self.google_login_counter = Counter(
-            "google_login_requests_total",
+            "gen3_google_login_requests_total",
             "Total number of Google login requests",
             registry=app.prometheus_registry,
         )
         self.ras_login_counter = Counter(
-            "fence_ras_login_requests_total",
+            "gen3_fence_ras_login_requests_total",
             "Total number of RAS login requests",
             registry=app.prometheus_registry,
         )
         self.presigned_urls_ga4gh_drs_counter = Counter(
-            "fence_presigned_urls_ga4gh_drs_requests_total",
+            "gen3_fence_presigned_urls_ga4gh_drs_requests_total",
             "Total number of presigned URL requests for GA4GH DRS",
             registry=app.prometheus_registry,
         )
         self.presigned_url_download_protocol_gcs_counter = Counter(
-            "fence_presigned_url_download_protocol_gcs_requests_total",
+            "gen3_fence_presigned_url_download_protocol_gcs_requests_total",
             "Total number of presigned URL requests for GCS",
             registry=app.prometheus_registry,
         )
         self.presigned_url_download_protocol_s3_counter = Counter(
-            "fence_presigned_url_download_protocol_s3_requests_total",
+            "gen3_fence_presigned_url_download_protocol_s3_requests_total",
             "Total number of presigned URL requests for S3",
             registry=app.prometheus_registry,
         )
         self.presigned_url_data_metrics_size_gauge = Gauge(
-            "fence_presigned_url_data_metrics_size_bytes",
+            "gen3_fence_presigned_url_data_metrics_size_bytes",
             "Size of data metrics in bytes",
             ["acl", "authz", "bucket", "user_sub", "client_id"],
             registry=app.prometheus_registry,
