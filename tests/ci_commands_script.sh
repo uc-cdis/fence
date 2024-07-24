e9345a4400ba4645547974ccd6fc5c6a141c1f87
Ed <emalinowski@uchicago.edu>
2024-07-24 15:13:52 -0500

    fix(metrics): Updated metrics code/tests

--- tests/ci_commands_script.sh
+++ tests/ci_commands_script.sh
@@ -2,5 +2,4 @@
 
 rm -Rf /var/tmp/uwsgi_flask_metrics/
 mkdir -p /var/tmp/uwsgi_flask_metrics/
-export PROMETHEUS_MULTIPROC_DIR="/var/tmp/uwsgi_flask_metrics/"
 poetry run pytest -vv --cov=fence --cov-report xml tests
