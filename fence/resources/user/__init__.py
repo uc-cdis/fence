fa680eb49518e27ecae0bbaee47d7b7ce9781b21
Kyle Burton <burtonk@uchicago.edu>
2024-07-26 19:53:15 -0500

    feat(PXP-11366): Remove passport from userinfo

--- fence/resources/user/__init__.py
+++ fence/resources/user/__init__.py
@@ -142,30 +142,6 @@ def get_user_info(current_session, username):
         optional_info = _get_optional_userinfo(user, requested_userinfo_claims)
         info.update(optional_info)
 
-    # Include ga4gh passport visas if access token has ga4gh_passport_v1 in scope claim
-    try:
-        encoded_access_token = flask.g.access_token or get_jwt_header()
-    except Unauthorized:
-        # This only happens if a session token was present (since login_required did not throw an error)
-        # but for some reason there was no access token in flask.g.access_token.
-        # (Perhaps it was manually deleted by the user.)
-        # In particular, a curl request made with no tokens shouldn't get here (bc of login_required).
-        # So the request is probably from a browser.
-        logger.warning(
-            "Session token present but no access token found. "
-            "Unable to check scopes in userinfo; some claims may not be included in response."
-        )
-        encoded_access_token = None
-
-    if encoded_access_token:
-        at_scopes = jwt.decode(
-            encoded_access_token,
-            algorithms=["RS256"],
-            options={"verify_signature": False},
-        ).get("scope", "")
-        if "ga4gh_passport_v1" in at_scopes:
-            info["ga4gh_passport_v1"] = []
-
     return info
 
 
