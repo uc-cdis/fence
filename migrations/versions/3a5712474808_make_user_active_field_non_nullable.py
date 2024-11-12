d76c071b6d78809d246cde4ff3e1e22537184492
pieterlukasse <pieterlukasse@gmail.com>
2024-11-12 20:07:25 +0100

    fix: improve migration logic for .active field
    based on this dicussion: https://github.com/uc-cdis/fence/pull/1189#discussion_r1838528076

--- migrations/versions/3a5712474808_make_user_active_field_non_nullable.py
+++ migrations/versions/3a5712474808_make_user_active_field_non_nullable.py
@@ -7,6 +7,8 @@ Create Date: 2024-11-08 22:00:41.161934
 """
 from alembic import op
 import sqlalchemy as sa
+from userdatamodel.models import User
+from sqlalchemy.orm import Session
 
 
 # revision identifiers, used by Alembic.
@@ -17,10 +19,18 @@ depends_on = None
 
 
 def upgrade():
-    op.execute('UPDATE "User" SET active = True WHERE active IS NULL')
+    conn = op.get_bind()
+    session = Session(bind=conn)
+    session.query(User)
+    active_users_count = session.query(User).filter(User.active.is_(True)).count()
+    if active_users_count > 0:
+        # if we have at least one user where "active" is explicitly set to "True", then we'll assume NULL is to mean "False":
+        op.execute('UPDATE "User" SET active = False WHERE active IS NULL')
+    else:
+        # else, we assume NULL means "True"
+        op.execute('UPDATE "User" SET active = True WHERE active IS NULL')
     op.alter_column("User", "active", nullable=False, server_default="True")
 
 
 def downgrade():
     op.alter_column("User", "active", nullable=True, server_default=None)
-    op.execute('UPDATE "User" SET active = NULL WHERE active = True')
