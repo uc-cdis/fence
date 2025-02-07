70f9f50f13e15f958a5f7220640a8530ddf87fe2
Albert Snow <ajsnow2012@gmail.com>
2025-02-07 15:00:49 -0600

    fix kid and command script

--- tests/ci_commands_script.sh
+++ tests/ci_commands_script.sh
@@ -2,4 +2,5 @@
 
 mkdir -p /var/tmp/uwsgi_flask_metrics/ || true
 export PROMETHEUS_MULTIPROC_DIR="/var/tmp/uwsgi_flask_metrics/"
-poetry run pytest -vv --cov=fence --cov-report xml tests
+echo "running tests"
+poetry run pytest -vv --cov=fence --cov-report xml .
