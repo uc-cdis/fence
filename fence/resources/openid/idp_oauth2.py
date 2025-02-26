baf42d46ddd3aa0dbbfe5ad71dc7db97dae17258
Guerdon Mukama <gmukama@gmail.com>
2025-02-26 20:13:48 +1100

    bug fix, use access_token

--- fence/resources/openid/idp_oauth2.py
+++ fence/resources/openid/idp_oauth2.py
@@ -125,16 +125,17 @@ class Oauth2ClientBase(object):
         verify_aud = self.settings.get("verify_aud", False)
         audience = self.settings.get("audience", self.settings.get("client_id"))
 
-        decoded_token = validate_jwt(
-            encoded_token=token["id_token"],
-            aud=audience,
-            scope=None,
-            issuers=[issuer],
-            purpose=None,
-            require_purpose=False,
-            options={"verify_aud": verify_aud, "verify_hash": False},
-            attempt_refresh=True,
-        )
+        if verify_aud:
+            decoded_token = validate_jwt(
+                encoded_token=token["access_token"],
+                aud=audience,
+                scope=None,
+                issuers=[issuer],
+                purpose=None,
+                require_purpose=False,
+                options={"verify_aud": verify_aud, "verify_hash": False},
+                attempt_refresh=True,
+            )
 
         return decoded_token, refresh_token
 
