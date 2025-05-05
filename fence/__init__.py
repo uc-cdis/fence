7bc4e476f8830e03dbc4ea54b553cf4759d17bd8
burtonk <117617405+k-burt-uch@users.noreply.github.com>
2025-05-05 16:17:13 -0500

    Clarity to default signer

--- fence/__init__.py
+++ fence/__init__.py
@@ -2,7 +2,7 @@
 import hashlib
 from itsdangerous import Signer
 
-Signer.default_digest_method = hashlib.sha256 # Explicitly set to sha256 as the default will break in FIPS environments when flask_wtf attempts to process a user registration form. 
+Signer.default_digest_method = hashlib.sha256 # Explicitly set to sha256 as the default (sha1) will break in FIPS environments when flask_wtf attempts to process a user registration form. 
 
 from collections import OrderedDict
 import os
