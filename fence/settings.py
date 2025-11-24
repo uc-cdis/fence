b40a424583ba0fe5d0290854b3c679af52f26dc9
Pauline Ribeyre <4224001+paulineribeyre@users.noreply.github.com>
2025-11-24 11:56:55 -0600

    Delete fence/settings.py

--- fence/settings.py
+++
@@ -1,9 +0,0 @@
-"""
-Fence Configuration Support Settings
-
-NOTE TO DEVELOPERS: This is NOT intended to be a place for configurable variables.
-                    Please put new cfg into the config-default.yaml
-
-This exists primarily as a legacy support for old configuration, but has been
-repurposed to hold some info used when searching for a provided configuration variable.
-"""
