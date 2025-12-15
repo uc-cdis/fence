4af5368653426f7fb7f9e5faf778309ce8b50688
Michael <lukowski9@gmail.com>
2025-12-15 14:10:37 -0600

    clean up imports

--- fence/blueprints/data/blueprint.py
+++ fence/blueprints/data/blueprint.py
@@ -11,7 +11,6 @@ from fence.blueprints.data.indexd import (
     BlankIndex,
     EmbeddingIndex,
     IndexedFile,
-    BulkIndexedFile,
     get_signed_url_for_file,
     bulk_get_signed_url_for_file,
     verify_data_upload_bucket_configuration,
