dfcba54a6ebb31914a237de8b859189b80eaa2e2
Kyle Burton <burtonk@uchicago.edu>
2026-05-13 22:15:40 -0500

    Add __passport endpoint

--- fence/blueprints/ga4gh.py
+++ fence/blueprints/ga4gh.py
@@ -1,10 +1,15 @@
 import flask
+from flask import current_app
+
+from fence.auth import login_required
 from fence.errors import UserError
 from fence.config import config
 
 from fence.blueprints.data.indexd import (
     get_signed_url_for_file,
 )
+from fence.models import UserPassport
+from fence.resources.user import get_current_user_info
 
 blueprint = flask.Blueprint("ga4gh", __name__)
 
@@ -43,3 +48,16 @@ def get_ga4gh_signed_url(object_id, access_id):
     )
 
     return flask.jsonify(result)
+
+
+@blueprint.route(
+    "/__passport",
+    methods=["GET", "POST"],
+)
+@login_required({"user"})
+def get_passport():
+    info = get_current_user_info()
+    user_id = info["user_id"]
+    db_session = current_app.scoped_session()
+    passport = db_session.query(UserPassport).filter_by(user_id=int(user_id)).first()
+    return flask.jsonify({"passport": passport})
